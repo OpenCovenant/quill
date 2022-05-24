961aa1a5c05625bae00d2020067a0c97c7e8053d
Andi Braimllari <andibraimllari@gmail.com>
2022-05-24 19:00:45 +0200

    change the version of @angular-devkit and run tests on a headless browser

--- karma.conf.js
+++ karma.conf.js
@@ -37,7 +37,7 @@ module.exports = function (config) {
         colors: true,
         logLevel: config.LOG_INFO,
         autoWatch: true,
-        browsers: ['Chrome'],
+        browsers: ['ChromeHeadless', 'Chrome'],
         singleRun: false,
         restartOnFileChange: true
     });
