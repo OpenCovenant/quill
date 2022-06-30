c73998de95e63118cd6e531d76b48e3674ed26b9
Andi Braimllari <andibraimllari@gmail.com>
2022-06-30 17:47:24 +0200

    rename quill-fe to quill

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
             suppressAll: true // removes the duplicated traces
         },
         coverageReporter: {
-            dir: require('path').join(__dirname, './coverage/quill-fe'),
+            dir: require('path').join(__dirname, './coverage/quill'),
             subdir: '.',
             reporters: [
                 {type: 'html'},
