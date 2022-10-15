c6bfd49fb37f5f6806765047f1d304d1d39f13c8
AndiBraimllari <AndiBraimllari@users.noreply.github.com>
2022-10-15 18:11:58 +0200

    show the count of some marking details (#75)

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,15 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import {Component} from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+
+import {environment} from "../../environments/environment";
 
 @Component({
-  selector: 'app-header',
-  templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+    selector: 'app-header',
+    templateUrl: './header.component.html',
+    styleUrls: ['./header.component.css']
 })
-export class HeaderComponent implements OnInit {
+export class HeaderComponent {
+    baseURL!: string;
+    getMarkingDetailsCount!: string;
+    markingDetailsCount!: number;
 
-  constructor() { }
+    constructor(private http: HttpClient) {
+        this.initializeURLs();
+        this.http.get(this.getMarkingDetailsCount).subscribe((data: any) => {
+            this.markingDetailsCount = data['count'];
+        });
+    }
 
-  ngOnInit(): void {
-  }
-
+    initializeURLs() {
+        this.baseURL = environment.baseURL;
+        this.getMarkingDetailsCount = this.baseURL + '/api/getMarkingDetailsCount';
+    }
 }
