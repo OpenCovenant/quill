b4229b41ea78ad4808de3b6c2a0ff45f4b0495aa
Andi Braimllari <andibraimllari@gmail.com>
2022-10-15 10:49:37 +0200

    dynamic detail fetching

--- src/app/marking-details/marking-details.component.ts
+++ src/app/marking-details/marking-details.component.ts
@@ -1,4 +1,8 @@
 import {Component} from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {ActivatedRoute} from "@angular/router";
+
+import {environment} from "../../environments/environment";
 
 @Component({
     selector: 'app-marking-details',
@@ -6,4 +10,22 @@ import {Component} from '@angular/core';
     styleUrls: ['./marking-details.component.css']
 })
 export class MarkingDetailsComponent {
+    baseURL!: string;
+    getMarkingDetailsURL!: string;
+
+    constructor(private route: ActivatedRoute, private http: HttpClient) {
+        this.initializeURLs();
+        this.route.paramMap.subscribe(paramMap => {
+            const id = paramMap.get('id');
+
+            this.http.get(this.getMarkingDetailsURL + '/' + id)
+                .subscribe((html: any) =>
+                    document.getElementById("markingDetails")!.innerHTML = html['marking_details_html']);
+        })
+    }
+
+    initializeURLs() {
+        this.baseURL = environment.baseURL;
+        this.getMarkingDetailsURL = this.baseURL + '/api/getMarkingDetails';
+    }
 }
