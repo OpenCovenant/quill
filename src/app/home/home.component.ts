b1a78a3ecaf7443e4b7f78ba2a293c7c6173e7f4
Andi Braimllari <>
2022-11-27 00:07:43 +0100

    merge two if-cases

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -368,10 +368,7 @@ export class HomeComponent implements AfterViewInit {
         }
 
         const textMarking: TextMarking | null = this.processedText ? this.processedText.textMarkings[textMarkingIndex] : null;
-        if (!textMarking) {
-            return this.EMPTY_STRING;
-        }
-        if (!textMarking.paragraph) {
+        if (!textMarking || !textMarking.paragraph) {
             return this.EMPTY_STRING;
         }
 
