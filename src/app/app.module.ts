51ef3715707ef5d8b06eb25d34918eb416ca6c19
andersonceci <andersonceci31@gmail.com>
2022-12-14 00:01:24 +0100

    added a new component wich uses google and facebook as login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,14 +2,14 @@ import {NgModule} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
 import {HttpClientModule} from '@angular/common/http';
 import {FormsModule} from "@angular/forms";
-
 import {AppComponent} from './app.component';
 import {MarkingDetailsComponent} from './marking-details/marking-details.component';
 import {AppRoutingModule} from "./app-routing.module";
 import {HeaderComponent} from './header/header.component';
 import {FooterComponent} from './footer/footer.component';
 import {HomeComponent} from './home/home.component';
-import { AuthenticationComponent } from './authentication/authentication.component';
+import {AuthenticationComponent} from './authentication/authentication.component';
+import {SocialAuthServiceConfig, SocialAuthService, FacebookLoginProvider,GoogleLoginProvider,SocialLoginModule} from '@abacritt/angularx-social-login';
 
 @NgModule({
     declarations: [
@@ -18,16 +18,40 @@ import { AuthenticationComponent } from './authentication/authentication.compone
         HeaderComponent,
         FooterComponent,
         HomeComponent,
-        AuthenticationComponent
+        AuthenticationComponent,
     ],
     imports: [
         BrowserModule,
         HttpClientModule,
         FormsModule,
-        AppRoutingModule
+        AppRoutingModule,
+        SocialLoginModule
     ],
-    providers: [],
-    bootstrap: [AppComponent]
-})
-export class AppModule {
+    providers: [
+        {
+            provide: 'SocialAuthServiceConfig',
+            useValue: {
+                autoLogin:false,
+                providers: [
+                    {
+                        id: GoogleLoginProvider.PROVIDER_ID,
+                        provider: new GoogleLoginProvider(
+                          'yourId'
+                        )
+                      },
+                    {
+                    id: FacebookLoginProvider.PROVIDER_ID,
+                    provider: new FacebookLoginProvider('yourId'),
+                    }
+                ],
+                onError:(err) => {
+                    console.log(err);
+                },
+            } as SocialAuthServiceConfig,
+        },
+        SocialAuthService,
+    ],
+      bootstrap: [AppComponent],
+    })
+    export class AppModule {
 }
--- src/app/authentication/authentication.component.ts
+++ src/app/authentication/authentication.component.ts
@@ -1,10 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FacebookLoginProvider, SocialAuthService, GoogleLoginProvider } from '@abacritt/angularx-social-login';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-authentication',
   templateUrl: './authentication.component.html',
   styleUrls: ['./authentication.component.css']
 })
-export class AuthenticationComponent {
+export class AuthenticationComponent implements OnInit{
+
+  public loggedIn!: Boolean;
+  public user: any;
+  private accessToken = '';
+
+  constructor(private authService:SocialAuthService, private httpClient: HttpClient){}
+  
+  ngOnInit(): void {
+    this.authService.authState.subscribe((user) => {
+      this.user = user 
+      this.loggedIn = user != null
+    })
+  }
+  signInWithGoogle(): void {
+    this.authService.signIn(GoogleLoginProvider.PROVIDER_ID);
+  }
+
+  signInWithFB(): void {
+    this.authService.signIn(FacebookLoginProvider.PROVIDER_ID);
+  }
+
+  signOut(): void {
+    this.authService.signOut();
+  }
+  refreshToken(): void {
+    this.authService.refreshAuthToken(GoogleLoginProvider.PROVIDER_ID);
+  }
+
+
 
 }
