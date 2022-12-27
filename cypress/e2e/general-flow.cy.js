88361e5227b3fede30f540b041b4fde43344ae59
Andi Braimllari <>
2022-12-28 00:07:49 +0100

    changes

--- cypress/e2e/general-flow.cy.js
+++ cypress/e2e/general-flow.cy.js
@@ -8,22 +8,14 @@ describe('a general flow of quill', () => {
     });
 
     it('will mark typos in the editor', () => {
-        cy.get("#editor").should("exist");
-        cy.get("#editor").children().should("exist");
-        cy.get("#editor>p").should("exist");
-        cy.get("#editor>p").first().should("not.have.attr", "class", "typo");
+        cy.get("#editor > p > .typo").should("not.exist");
         cy.get("#editor").type("asd ");
-        cy.wait(5000);
-        cy.get("#editor>p>.typo").should("exist");
-        // cy.get("#editor>p").first().should("have.attr", "class", "typo");
+        cy.get("#editor > p > .typo").should("exist");
     });
 
     it('will mark loanwords in the editor', () => {
-        cy.wait(5000);
         cy.get("#editor > p > .loanword").should("not.exist");
         cy.get("#editor").type("lider ");
-        cy.wait(5000);
-        cy.get("#editor > p").children().should("exist");
+        cy.get("#editor > p > .loanword").should("exist");
     });
 });
-
