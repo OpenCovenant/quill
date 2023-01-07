af589e25c00c67e9856630fed6d2139023238c06
andersonceci <andersonceci31@gmail.com>
2023-01-07 21:58:14 +0100

    Added few more tests also different file tests

--- cypress/e2e/general-flow.cy.js
+++ cypress/e2e/general-flow.cy.js
@@ -18,19 +18,47 @@ describe('a general flow of quill', () => {
         cy.get("#editor").type("lider ");
         cy.get("#editor > p > .loanword").should("exist");
     });
-    
-    it('will click on button and will check for loanword in output conainer', () =>{
+
+    it('will click on button and will check for loanword in output conainer on WORD file', () =>{
         
         cy.get('button[id="uploadDocumentToggleButton"]').click();
         cy.get('.btn-primary').contains('SHTYP KËTU ').click().selectFile('cypress/fixtures/test.docx');
-        cy.get("span.loanword").contains('ters').should("exist");
+        cy.get("span.loanword").contains('lider').should("exist");
     });
 
-    it('will click on button and will check for loanword in output conainer', () =>{
+    it('will click on button and will check for loanword in output conainer on WORD file', () =>{
         
         cy.get('button[id="uploadDocumentToggleButton"]').click();
         cy.get('.btn-primary').contains('SHTYP KËTU ').click().selectFile('cypress/fixtures/test.docx');
-        cy.get("span.typo").contains('asds').should("exist");
+        cy.get("span.typo").contains('asd').should("exist");
+    });
+
+    it('will click on button and will check for loanword in output conainer on PDF file', () =>{
+        
+        cy.get('button[id="uploadDocumentToggleButton"]').click();
+        cy.get('.btn-primary').contains('SHTYP KËTU ').click().selectFile('cypress/fixtures/test.pdf');
+        cy.get("span.loanword").contains('lider').should("exist");
+    });
+
+    it('will click on button and will check for loanword in output conainer on PDF file', () =>{
+        
+        cy.get('button[id="uploadDocumentToggleButton"]').click();
+        cy.get('.btn-primary').contains('SHTYP KËTU ').click().selectFile('cypress/fixtures/test.pdf');
+        cy.get("span.typo").contains('asd').should("exist");
+    });
+
+    it('will click on button and will check for loanword in output conainer on LIBRA file', () =>{
+        
+        cy.get('button[id="uploadDocumentToggleButton"]').click();
+        cy.get('.btn-primary').contains('SHTYP KËTU ').click().selectFile('cypress/fixtures/test.odt');
+        cy.get("span.loanword").contains('lider').should("exist");
+    });
+
+    it('will click on button and will check for loanword in output conainer on LIBRA file', () =>{
+        
+        cy.get('button[id="uploadDocumentToggleButton"]').click();
+        cy.get('.btn-primary').contains('SHTYP KËTU ').click().selectFile('cypress/fixtures/test.odt');
+        cy.get("span.typo").contains('asd').should("exist");
     });
 
     it('will click on editor and check if changes are made on textfield for loanwords', () => {
@@ -148,9 +176,19 @@ describe('a general flow of quill', () => {
     it('will test if  Opening and closing the offcanvas works as expected.', () => {
 
         cy.get('.navbar-toggler-icon').click();
-        cy.get('.list-group-item-action').contains("Rreth Nesh").should("exist");
-        cy.get('.list-group-item-action').contains("Rreth Penda").should("exist");
+        cy.get('.offcanvas.offcanvas-start.show').should("exist");
         cy.get('#offcanvasCloseButton').click();
+        
+    }); 
+
+    it("will test if Clicking on the information button of some markings that lead to pages with further details on the markings. ", () => {
+
+        cy.get("#editor").type("Shkoi tek zyra. ");
+        cy.get('.bi.bi-info-circle').click();
+        cy.url().should('include', '/te-dhe-tek');
+
+
 
     });
+
 }); 
