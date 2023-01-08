970f1cef3dfd3ea0e9a0201a9086723a5a038207
Andi Braimllari <>
2023-01-08 20:20:02 +0100

    changes

--- mock-server/index.js
+++ mock-server/index.js
@@ -1,5 +1,11 @@
 const express = require("express");
+const cors = require('cors');
 const app = express();
+
+app.use(cors());
+// app.use(express.urlencoded());
+app.use(express.json());
+
 app.listen(3000, () => {
     console.log("The mock-server is currently running on port 3000 ...");
 });
@@ -12,15 +18,20 @@ app.get('/api/ping', (req, res, next) => {
     res.json(["a", "b", "c", "d", "e"]);
 });
 
-app.get('/api/generateMarkings', (req, res, next) => {
+app.post('/api/generateMarkings', (req, res, next) => {
     res.json(["a", "b", "c", "d", "e"]);
 });
 
-app.get('/api/generateMarkingsForParagraphs', (req, res, next) => {
+app.post('/api/generateMarkingsForParagraphs', (req, res, next) => {
+    // if
+    // console.log(req)
+    console.log(req.body)
+    console.log(req.params)
     res.json(["a", "b", "c", "d", "e"]);
 });
 
-app.get('/api/getMarkingDetails/:marking_details_id', (req, res, next) => {
+app.post('/api/getMarkingDetails/:marking_details_id', (req, res, next) => {
+    const markingDetailsID = req.body;
     res.json(["a", "b", "c", "d", "e"]);
 });
 
