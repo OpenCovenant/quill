a579dea7cc820f0d55b6d75214a3c2b29aa5f596
Andi Braimllari <>
2023-01-08 19:50:42 +0100

    changes

--- mock-server/index.js
+++ mock-server/index.js
@@ -7,3 +7,31 @@ app.listen(3000, () => {
 app.get("/url", (req, res, next) => {
     res.json(["a", "b", "c", "d", "e"]);
 });
+
+app.get('/api/ping', (req, res, next) => {
+    res.json(["a", "b", "c", "d", "e"]);
+});
+
+app.get('/api/generateMarkings', (req, res, next) => {
+    res.json(["a", "b", "c", "d", "e"]);
+});
+
+app.get('/api/generateMarkingsForParagraphs', (req, res, next) => {
+    res.json(["a", "b", "c", "d", "e"]);
+});
+
+app.get('/api/getMarkingDetails/:marking_details_id', (req, res, next) => {
+    res.json(["a", "b", "c", "d", "e"]);
+});
+
+app.get('/api/getMarkingTypes', (req, res, next) => {
+    res.json(["a", "b", "c", "d", "e"]);
+});
+
+app.get('/api/getMarkingTypesCount', (req, res, next) => {
+    res.json(["a", "b", "c", "d", "e"]);
+});
+
+app.get('/api/uploadDocument', (req, res, next) => {
+    res.json(["a", "b", "c", "d", "e"]);
+});
