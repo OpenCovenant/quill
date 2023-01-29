5e4aa52a766ae17727bacd0b1128fda214a7a329
Andi Braimllari <>
2023-01-29 19:44:32 +0100

    changes

--- cypress/e2e/hiq-button.cy.js
+++ cypress/e2e/hiq-button.cy.js
@@ -11,11 +11,12 @@ describe("test for button hiq", () => {
     });
 
     it("will click on `HIQ` buttons and check if it deletes the markings in the editor", () => {
+        const text = "gabmim gabmim "
         cy.get("#editor > p > .typo").should("not.exist");
-        cy.get("#editor").type("gabmim gabmim ");
+        cy.get("#editor").type(text);
         cy.get("button.btn-outline-danger").first().contains("HIQ").click();
         cy.get("button.btn-outline-danger").first().contains("HIQ").click();
-        // cy.get("button.btn-outline-danger").contains("HIQ").click();
         cy.get("button.suggestion").should("not.exist");
+        // cy.get("#editor").should('contain.text', text)
     });
 });
