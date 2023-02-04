bef80425cfb71741b939bcb677afaf466647230c
Andi Braimllari <>
2023-02-04 16:37:07 +0100

    Cypress changes

--- cypress/e2e/write-to-editor-and-upload-document.cy.js
+++ cypress/e2e/write-to-editor-and-upload-document.cy.js
@@ -4,6 +4,8 @@ describe("a general flow of quill", () => {
     });
 
     it("contains the editor in which we want to write", () => {
-        cy.get("[data-test='editor']").should("exist");
+        cy.get("[data-test='placeholder']").should('exist');
+        cy.get("[data-test='editor'").type("gabmim ");
+        cy.get("[data-test='write-text-toggle']").should("exist").click();
     });
 });
