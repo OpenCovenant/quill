8710da4ed2699423a26f9f06dcca825fdd07e1f0
Andi Braimllari <>
2023-02-04 10:20:34 +0100

    Cypress changes

--- cypress/e2e/hiq-button.cy.js
+++ cypress/e2e/hiq-button.cy.js
@@ -14,10 +14,14 @@ describe("test for button hiq", () => {
         const text = "gabmim gabmim ";
         cy.get("[data-test='editor'] > p > .typo").should("not.exist");
         cy.get("[data-test='editor']").type(text);
+        cy.wait(2000)
         cy.get("button.btn-outline-danger").first().contains("HIQ").click();
+        cy.wait(2000)
         cy.get("button.btn-outline-danger").first().contains("HIQ").click();
+        // cy.get("button.btn-outline-danger").first().contains("HIQ").click();
         cy.get("button.suggestion").should("not.exist");
         // I guess because the whitespaces are transformed into NBSPs
+        console.log(text.charCodeAt(6), text.replace(/ /g, '\u00a0').charCodeAt(6))
         cy.get("[data-test='editor']").should('contain.text', text.replace(/ /g, '\u00a0'));
     });
 });
