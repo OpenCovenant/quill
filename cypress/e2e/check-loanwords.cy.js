b72bfa08741c441124059fdee93b45a80679affc
AndiBraimllari <AndiBraimllari@users.noreply.github.com>
2023-04-19 15:39:21 +0200

    Improve cypress (#151)
    * improve and fix cypress tests
    
    * changes to package-log
    
    * changes
    
    * remove wait-on
    
    ---------
    
    Co-authored-by: Andi Braimllari <>

--- cypress/e2e/check-loanwords.cy.js
+++ cypress/e2e/check-loanwords.cy.js
@@ -5,21 +5,21 @@ describe("click on editor and check if changes are made on textfield for loanwor
 
     it("will click on editor and check if changes are made on textfield for loanwords", () => {
         cy.get("#editor > p > .loanword").should("not.exist");
-        cy.get("#editor").type("lider ");
+        cy.get('[data-test="editor"]').type("lider ");
         cy.get("button.suggestion").contains("prijës").click();
-        cy.get("#editor").contains("prijës").should("exist");
-        cy.get("#editor").clear();
+        cy.get('[data-test="editor"]').contains("prijës").should("exist");
+        cy.get('[data-test="editor"]').clear();
 
         cy.get("#editor > p > .loanword").should("not.exist");
-        cy.get("#editor").type("lider ");
+        cy.get('[data-test="editor"]').type("lider ");
         cy.get("button.suggestion").contains("drejtues").click();
-        cy.get("#editor").contains("drejtues").should("exist");
-        cy.get("#editor").clear();
+        cy.get('[data-test="editor"]').contains("drejtues").should("exist");
+        cy.get('[data-test="editor"]').clear();
 
         cy.get("#editor > p > .loanword").should("not.exist");
-        cy.get("#editor").type("lider ");
+        cy.get('[data-test="editor"]').type("lider ");
         cy.get("button.suggestion").contains("udhëheqës").click();
-        cy.get("#editor").contains("udhëheqës").should("exist");
-        cy.get("#editor").clear();
+        cy.get('[data-test="editor"]').contains("udhëheqës").should("exist");
+        cy.get('[data-test="editor"]').clear();
     });
 });
--- cypress/e2e/check-typos.cy.js
+++ cypress/e2e/check-typos.cy.js
@@ -5,27 +5,27 @@ describe("click on editor and check if changes are made on textfield for typos",
 
     it("will click on editor and check if changes are made on textfield for typos", () => {
         cy.get("#editor > p > .typo").should("not.exist");
-        cy.get("#editor").type("gabmim ");
+        cy.get('[data-test="editor"]').type("gabmim ");
         cy.get("button.suggestion").contains("gabime").click();
-        cy.get("#editor").contains("gabime").should("exist");
-        cy.get("#editor").clear();
+        cy.get('[data-test="editor"]').contains("gabime").should("exist");
+        cy.get('[data-test="editor"]').clear();
 
         cy.get("#editor > p > .typo").should("not.exist");
-        cy.get("#editor").type("gabmim ");
+        cy.get('[data-test="editor"]').type("gabmim ");
         cy.get("button.suggestion").contains("gabimi").click();
-        cy.get("#editor").contains("gabimi").should("exist");
-        cy.get("#editor").clear();
+        cy.get('[data-test="editor"]').contains("gabimi").should("exist");
+        cy.get('[data-test="editor"]').clear();
 
         cy.get("#editor > p > .typo").should("not.exist");
-        cy.get("#editor").type("gabmim ");
+        cy.get('[data-test="editor"]').type("gabmim ");
         cy.get("button.suggestion").contains("gazmim").click();
-        cy.get("#editor").contains("gazmim").should("exist");
-        cy.get("#editor").clear();
+        cy.get('[data-test="editor"]').contains("gazmim").should("exist");
+        cy.get('[data-test="editor"]').clear();
 
         cy.get("#editor > p > .typo").should("not.exist");
-        cy.get("#editor").type("gabmim ");
+        cy.get('[data-test="editor"]').type("gabmim ");
         cy.get("button.suggestion").contains("gabim").click();
-        cy.get("#editor").contains("gabim").should("exist");
-        cy.get("#editor").clear();
+        cy.get('[data-test="editor"]').contains("gabim").should("exist");
+        cy.get('[data-test="editor"]').clear();
     });
 });
