03092e413935a5a775399e2bff33029ad129d6e5
Andi Braimllari <>
2023-04-19 15:12:48 +0200

    improve and fix cypress tests

--- cypress/e2e/hiq-button.cy.js
+++ cypress/e2e/hiq-button.cy.js
@@ -5,19 +5,18 @@ describe("test for button hiq", () => {
 
     it("will click on hiq button and check if it deletes the text on editor", () => {
         cy.get("#editor > p > .typo").should("not.exist");
-        cy.get("#editor").type("gabmim ");
-        cy.get("button.btn-outline-danger").contains("HIQ").click();
+        cy.get('[data-test="editor"]').type("gabmim ");
+        cy.get('[data-test="dismiss-marking-button"]').contains("HIQ").click();
         cy.get("button.suggestion").should("not.exist");
     });
 
     it("will click on `HIQ` buttons and check if it deletes the markings in the editor", () => {
-        const text = "gabmim gabmim ";
+        const text = "gabmim gabmim";
         cy.get("#editor > p > .typo").should("not.exist");
-        cy.get("#editor").type(text);
-        cy.get("button.btn-outline-danger").first().contains("HIQ").click();
-        cy.get("button.btn-outline-danger").first().contains("HIQ").click();
+        cy.get('[data-test="editor"]').type(text);
+        cy.get('[data-test="dismiss-marking-button"]').first().contains("HIQ").click();
+        cy.get('[data-test="dismiss-marking-button"]').first().contains("HIQ").click();
         cy.get("button.suggestion").should("not.exist");
-        // I guess because the whitespaces are transformed into NBSPs
-        cy.get("#editor").should('contain.text', text.replace(/ /g, '\u00a0'));
+        cy.get('[data-test="editor"]').should('have.text', text);
     });
 });
