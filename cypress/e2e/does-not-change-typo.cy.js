ba808cb00455571acb0dd8ee14b702bbf152f1d2
Anderson Ceci <andersonceci31@gmail.com>
2023-04-21 00:56:50 +0200

    added wait time to test if the test fails

--- cypress/e2e/does-not-change-typo.cy.js
+++ cypress/e2e/does-not-change-typo.cy.js
@@ -6,6 +6,7 @@ describe("its going to test if the typo is going to change its value once the do
     it("will click on dokument and on shkruaj again", () => {
         cy.get("#editor > p > .typo").should("not.exist");
         cy.get('[data-test="editor"]').type("asd ");
+        cy.wait(2000);
         cy.get('button[id="uploadDocumentToggleButton"]').click();
         cy.get("span.typo").contains("asd").should("exist");
         cy.get('button[id="writeTextToggleButton"]').click();
