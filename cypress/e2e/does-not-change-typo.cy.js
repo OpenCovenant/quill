61e4ec0d65a6ec1fdf3637d608760b5da3a24616
Anderson Ceci <andersonceci31@gmail.com>
2023-04-22 00:16:12 +0200

    replaced asd with word gabmim and removed it from mocking server

--- cypress/e2e/does-not-change-typo.cy.js
+++ cypress/e2e/does-not-change-typo.cy.js
@@ -5,12 +5,12 @@ describe("its going to test if the typo is going to change its value once the do
 
     it("will click on dokument and on shkruaj again", () => {
         cy.get("#editor > p > .typo").should("not.exist");
-        cy.get('[data-test="editor"]').type("asd ");
+        cy.get('[data-test="editor"]').type("gabmim ");
         cy.wait(2001);
         cy.get('button[id="uploadDocumentToggleButton"]').click();
-        cy.get("span.typo").contains("asd").should("exist");
+        cy.get("span.typo").contains("gabmim").should("exist");
         cy.get('button[id="writeTextToggleButton"]').click();
-        cy.get('[data-test="editor"]').contains("asd").should("exist");
+        cy.get('[data-test="editor"]').contains("gabmim").should("exist");
         cy.get('[data-test="editor"]').clear();
         });
     });
--- cypress/e2e/placeholder-does-not-show-after-clicking-dockument.cy.js
+++ cypress/e2e/placeholder-does-not-show-after-clicking-dockument.cy.js
@@ -5,7 +5,7 @@ describe("its going to test if the place holder is there after clicking on dokum
 
     it("will click on dokument and on shkruaj again and check for the placeholder if its still there", () => {
         cy.get("#editor > p > .typo").should("not.exist");
-        cy.get('[data-test="editor"]').type("asd ");
+        cy.get('[data-test="editor"]').type("gabmim ");
         cy.get('button[id="uploadDocumentToggleButton"]').click();
         cy.get('button[id="writeTextToggleButton"]').click();
         cy.get('[data-test="editor-placeholder"]').should("not.exist");
