8b448f980b9ddf235cb133cdf0abfbde0f73a63a
Andi Braimllari <>
2023-04-22 12:02:01 +0200

    refactor

--- cypress/e2e/copy-button.cy.js
+++ cypress/e2e/copy-button.cy.js
@@ -6,7 +6,7 @@ describe("going to test the copy button and paste it on editor", () => {
     it("will click on copy button and paste on editor", () => {
         cy.get('[data-test="editor"]').clear().type("test per butonin copy");
 
-        cy.get("#copyToClipboardButton").click();
+        cy.get('[data-test="copy-to-clipboard-button"]').click();
 
         cy.window().then((win) => {
             win.navigator.clipboard.readText().then((text) => {
