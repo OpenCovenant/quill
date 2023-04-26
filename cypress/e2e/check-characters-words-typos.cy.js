f9b63782f01950fbb4e609442b6273976fcd72ae
Anderson Ceci <andersonceci31@gmail.com>
2023-04-26 22:03:03 +0200

    added another test that shows default markings when homepage is loaded

--- cypress/e2e/check-characters-words-typos.cy.js
+++ cypress/e2e/check-characters-words-typos.cy.js
@@ -3,7 +3,7 @@ describe("its going to test if the characters change, words, typos change as we 
         cy.visit("/");
     });
 
-    it("will click on dokument and on shkruaj again", () => {
+    it("will correctly update karaktere, fjalë, shenjime", () => {
         cy.get("#editor > p > .typo").should("not.exist");
         cy.get('[data-test="editor"]').type("gabmim ");
         cy.wait(2000);
@@ -14,6 +14,8 @@ describe("its going to test if the characters change, words, typos change as we 
         cy.get('[data-test="editor"]').clear();
         cy.get('#characterCountSpan').contains("0 karaktere, 0 fjalë, 0 shenjime").should("exist");
         cy.get('[data-test="editor"]').type('{shift}');
-         cy.get('#characterCountSpan').contains("0 karaktere, 0 fjalë, 0 shenjime").should("exist");
+        cy.get('#characterCountSpan').contains("0 karaktere, 0 fjalë, 0 shenjime").should("exist");
+        cy.get('[data-test="editor"]').type('{enter}');
+        cy.get('#characterCountSpan').contains("0 karaktere, 0 fjalë, 0 shenjime").should("exist");
     });
 });
