3f1993c322ac3f3b5c843386bc9c95daf34c67b9
Andi Braimllari <>
2023-04-27 23:01:52 +0200

    update test

--- cypress/e2e/the-order-of-markings.cy.js
+++ cypress/e2e/the-order-of-markings.cy.js
@@ -4,11 +4,8 @@ describe("will check the order of markings are in the right order as we typed th
     });
 
     it("will check the marking order as written on text box", () => {
-        cy.get('[data-test="editor"]').type("lider ");
-        cy.get("span.loanword").eq(0).should('contain', 'lider');
-        cy.get('[data-test="editor"]').type("gabmim ");
-        cy.get("span.typo").eq(0).should('contain', 'gabmim');
-        cy.get('[data-test="editor"]').type("Shkoi tek zyra.");
-        cy.get("span.typo").eq(1).should('contain', 'tek');
+        cy.get('[data-test="editor"]').type("lider gabmim");
+        cy.get("span.loanword").eq(0).should("have.text", "lider");
+        cy.get("span.typo").eq(0).should("have.text", "gabmim");
     });
 });
