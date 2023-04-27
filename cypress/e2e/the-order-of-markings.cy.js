fce79c5d391e3daaa830e174123b28e5ef6f1280
Anderson Ceci <andersonceci31@gmail.com>
2023-04-27 22:45:49 +0200

    for some reasons eq(2) doesnt grab tek

--- cypress/e2e/the-order-of-markings.cy.js
+++ cypress/e2e/the-order-of-markings.cy.js
@@ -5,10 +5,10 @@ describe("will check the order of markings are in the right order as we typed th
 
     it("will check the marking order as written on text box", () => {
         cy.get('[data-test="editor"]').type("lider ");
-        cy.get("span.loanword").contains("lider").should("exist");
+        cy.get("span.loanword").eq(0).should('contain', 'lider');
         cy.get('[data-test="editor"]').type("gabmim ");
-        cy.get("span.typo").contains("gabmim").should("exist");
+        cy.get("span.typo").eq(1).should('contain', 'gabmim');
         cy.get('[data-test="editor"]').type("Shkoi tek zyra.");
-        cy.get("span.typo").contains("tek").should("exist");
+        cy.get("span.typo").eq(3).should('contain', 'tek');
     });
 });
