cd30256e842046ab3c1b9d7221be172777f841f1
Andi Braimllari <>
2023-04-27 22:56:19 +0200

    change text

--- cypress/e2e/the-order-of-markings.cy.js
+++ cypress/e2e/the-order-of-markings.cy.js
@@ -4,9 +4,8 @@ describe("will check the order of markings are in the right order as we typed th
     });
 
     it("will check the marking order as written in the editor", () => {
-        cy.get('[data-test="editor"]').type("lider gabmim Shkoi tek zyra.");
+        cy.get('[data-test="editor"]').type("lider gabmim");
         cy.get("span.loanword").eq(0).should("have.text", "lider");
         cy.get("span.typo").eq(0).should("have.text", "gabmim");
-        cy.get("span.typo").eq(1).should("have.text", "tek");
     });
 });
