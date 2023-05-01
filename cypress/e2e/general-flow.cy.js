1bb65f1bede6e2793075849402a0578e42512f96
Andi Braimllari <>
2023-05-02 01:52:40 +0200

    add cypress test for collapse/expand arrows, refactor

--- cypress/e2e/general-flow.cy.js
+++ cypress/e2e/general-flow.cy.js
@@ -8,20 +8,43 @@ describe("a general flow of quill", () => {
     });
 
     it("will mark typos in the editor", () => {
-        cy.get("#editor > p > .typo").should("not.exist");
+        cy.get('[data-test="editor"] > p > .typo').should("not.exist");
         cy.get('[data-test="editor"]').type("gabmim ");
-        cy.get("#editor > p > .typo").should("exist");
+        cy.get('[data-test="editor"] > p > .typo').should("exist");
     });
 
     it("will mark loanwords in the editor", () => {
         cy.get('[data-test="editor"] > p > .loanword').should("not.exist");
         cy.get('[data-test="editor"]').type("lider ");
-        cy.get("#editor > p > .loanword").should("exist");
+        cy.get('[data-test="editor"] > p > .loanword').should("exist");
     });
 
     it("will test if  Opening and closing the offcanvas works as expected.", () => {
         cy.get('[data-test="navbar-toggler-icon"]').click();
         cy.get(".offcanvas.offcanvas-start.show").should("exist");
         cy.get('[data-test="close-offcanvas-button"]').click();
+    });
+
+    it("will click on the expand/collapse arrows and then choose on a suggestion", () => {
+        cy.get('[data-test="editor"]').type("eshte");
+        cy.get('[data-test="suggestion"]').children().should("have.length", 4);
+        cy.get(
+            '[data-test="oscillate-suggestions-button"].bi-arrow-right-square'
+        ).click();
+        cy.get('[data-test="suggestion"]')
+            .children()
+            .should("have.length.gt", 4);
+        cy.get(
+            '[data-test="oscillate-suggestions-button"].bi-arrow-left-square'
+        ).click();
+        cy.get('[data-test="suggestion"]').children().should("have.length", 4);
+        cy.get(
+            '[data-test="oscillate-suggestions-button"].bi-arrow-right-square'
+        ).click();
+        cy.get('[data-test="suggestion"]')
+            .children()
+            .should("have.length.gt", 4);
+        cy.get('[data-test="suggestion"]').contains("është").click();
+        cy.get('[data-test="editor"]').should("have.text", "është");
     });
 });
