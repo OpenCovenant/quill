d4dd5aa0b8da9e730a4263d0ccbe28f96f208ab2
Anderson Ceci <andersonceci31@gmail.com>
2023-05-06 00:48:05 +0200

    fixed the bug and ready to merge

--- cypress/e2e/automatically-focus-on-editor.cy.js
+++ cypress/e2e/automatically-focus-on-editor.cy.js
@@ -1,23 +1,23 @@
-    describe('it will check if the focus is on editor on desktop and not in mobiles', () => {
-        context('720p resolution', () => {
-          beforeEach(() => {
+describe("it will check if the focus is on editor on desktop and not in mobiles", () => {
+    context("720p resolution", () => {
+        beforeEach(() => {
+            cy.viewport(1280, 720);
             cy.visit("/");
-            cy.viewport(1280, 720)
-          })
-      
-          it('tests on resolution 1280-720 (desktop)', () => {
-            cy.focused().should('have.attr', 'id', 'editor');
-          })
-        })
-      
-        context('iphone-7', () => {
-          beforeEach(() => {
+        });
+
+        it("tests on resolution 1280-720 (desktop)", () => {
+            cy.focused().should("have.attr", "id", "editor");
+        });
+    });
+
+    context("iphone-7", () => {
+        beforeEach(() => {
+            cy.viewport("iphone-7");
             cy.visit("/");
-            cy.viewport('iphone-7')
-          })
-      
-          it('tests on iphone 7 resolution 375-667', () => {
-            cy.focused().should('not.have.attr', 'id', 'editor');
-          })
-        })
-      })
+        });
+
+        it("tests on iphone 7 resolution 375-667", () => {
+            cy.focused().should("not.exist");
+        });
+    });
+});
