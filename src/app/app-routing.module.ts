c8b089607338add26b9f333456a3a66766b6b245
Anderson Ceci <andersonceci31@gmail.com>
2023-07-13 20:51:45 +0200

    Commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,8 +2,10 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { MarkingDetailsComponent } from './marking-details/marking-details.component';
 import { HomeComponent } from './home/home.component';
+import { SettingsComponent } from './settings/settings.component';
 
 const routes: Routes = [
+    {path: 'settings', component:SettingsComponent},
     { path: ':id', component: MarkingDetailsComponent },
     { path: '', component: HomeComponent }
 ];
