31094152ecfd5a7f6991eeab9414e9f8ec72ab16
Anderson Ceci <andersonceci31@gmail.com>
2023-07-13 23:29:41 +0200

    Added the full List of Text markings also grabed the enabled status

---
+++ src/app/settings/settings.component.ts
@@ -0,0 +1,38 @@
+import { Component, OnInit } from '@angular/core';
+import {HttpClient, HttpHeaders} from "@angular/common/http";
+
+@Component({
+    selector: 'app-settings',
+    templateUrl: './settings.component.html',
+    styleUrls: ['./settings.component.css']
+})
+export class SettingsComponent implements OnInit {
+    results: any;
+    markingTypes: any[] = [];
+
+    constructor(private http: HttpClient) {
+    }
+
+    ngOnInit(): void {
+        this.getApiResponse();
+    }
+
+    getApiResponse() {
+        this.http.get<any>(
+            // API Link
+            '[URL]').subscribe(response => {
+            this.results = response;
+            this.markingTypes = Object.values(this.results.marking_types);
+            for (const markingType in this.results.marking_types) {
+                if (this.results.marking_types.hasOwnProperty(markingType)) {
+                    this.results.marking_types[markingType].description;
+                }
+            }
+            for (const markingType in this.results.marking_types) {
+                if (this.results.marking_types.hasOwnProperty(markingType)) {
+                    this.results.marking_types[markingType].enabled;
+                }
+            }
+        });
+    }
+}
