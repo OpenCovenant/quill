c176a4612f8cec17498121519c6d989793aef0ac
Andi Braimllari <>
2023-07-22 20:26:40 +0200

    changes

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -1,8 +1,12 @@
 import { Component } from '@angular/core';
+import { DarkModeService } from '../dark-mode.service'
 
 @Component({
     selector: 'app-footer',
     templateUrl: './footer.component.html',
     styleUrls: ['./footer.component.css']
 })
-export class FooterComponent {}
+export class FooterComponent {
+    constructor(public darkModeService: DarkModeService) {
+    }
+}
