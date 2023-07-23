1c2f999ba9b8bb2a15a4c75972d69bd22f94c1ea
Andi Braimllari <>
2023-07-23 23:30:06 +0200

    general refactoring

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -101,7 +101,7 @@ export class HomeComponent implements AfterViewInit, OnDestroy {
             // TODO some browsers still seem to use this deprecated method, keep it around for some more time
             minWidthMatchMedia.addListener(this.focusOnMediaMatch);
         }
-        (document.getElementById('flexSwitchCheckChecked') as any).checked =
+        (document.getElementById('flex-switch-check-checked') as HTMLInputElement).checked =
             this.localStorageService.canStoreWrittenTexts;
 
         this.fromKeyupEvent$ = fromEvent(
@@ -424,7 +424,7 @@ export class HomeComponent implements AfterViewInit, OnDestroy {
 
     copyToClipboard(): void {
         const copyToClipboardButton: HTMLElement = document.getElementById(
-            'copyToClipboardButton'
+            'copy-to-clipboard-button'
         )!;
         copyToClipboardButton.classList.replace(
             'bi-clipboard',
@@ -449,7 +449,7 @@ export class HomeComponent implements AfterViewInit, OnDestroy {
                 select.addRange(range);
                 document.execCommand('copy');
                 select.removeAllRanges();
-            } else {
+            } else { // NOTE: this part might only be for IE
                 range = (document.body as any).createTextRange();
                 range.moveToElementText(editor);
                 range.select();
@@ -468,7 +468,7 @@ export class HomeComponent implements AfterViewInit, OnDestroy {
 
     toggleStoringOfWrittenTexts(): void {
         this.localStorageService.toggleWritingPermission(
-            (document.getElementById('flexSwitchCheckChecked') as any).checked
+            (document.getElementById('flex-switch-check-checked') as HTMLInputElement).checked
         );
     }
 
@@ -485,7 +485,7 @@ export class HomeComponent implements AfterViewInit, OnDestroy {
      */
     placeWrittenText(writtenText: string): void {
         document.getElementById(this.EDITOR_KEY)!.innerText = writtenText;
-        document.getElementById('closeWrittenTextsModalButton')!.click();
+        document.getElementById('close-written-texts-modal-button')!.click();
         this.markEditor();
         this.updateCharacterAndWordCount();
     }
@@ -843,8 +843,7 @@ export class HomeComponent implements AfterViewInit, OnDestroy {
     }
 
     disableEditor(): void {
-        (document.getElementById(this.EDITOR_KEY) as any)!.contentEditable =
-            false;
+        (document.getElementById(this.EDITOR_KEY) as HTMLDivElement).contentEditable = 'false';
 
         document.getElementById(this.PLACEHOLDER_ELEMENT_ID)!.innerText =
             'Fatkeqësisht kemi një problem me serverat. Ju kërkojmë ndjesë, ndërsa kërkojme për një zgjidhje.';
@@ -859,8 +858,8 @@ export class HomeComponent implements AfterViewInit, OnDestroy {
 
     listenForMarkingFocus(): void {
         const textMarkings = document.querySelectorAll('#editor > p > .typo');
-        textMarkings.forEach((node: any, index: number) =>
-            node.addEventListener(
+        textMarkings.forEach((element: Element, index: number) =>
+            element.addEventListener(
                 'click',
                 this.focusRightSideMarking.bind(this, index)
             )
