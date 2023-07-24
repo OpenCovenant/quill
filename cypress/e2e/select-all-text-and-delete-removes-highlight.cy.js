842b5f808c491a2d743b5ee5f15eb79c5369c4f7
Anderson Ceci <andersonceci31@gmail.com>
2023-07-25 01:05:35 +0200

    Third Test Selecting all text and deleting it removes the selected highlighted marking

---
+++ cypress/e2e/select-all-text-and-delete-removes-highlight.cy.js
@@ -0,0 +1,19 @@
+describe("Removing the highlighted marking by selecting all the text and deleting it, actually removes the highlighted marking.", () => {
+    beforeEach(() => {
+        cy.visit("/");
+    });
+
+    it("will select all text and delete and check if the highlighted marking is removed", () => {
+        cy.get("#editor > p > .typo").should("not.exist");
+        cy.get('[data-test="editor"]').type("asd ");
+        cy.get('[data-test="editor"]').type(" gabmim ");
+        cy.get('[data-test="editor"]').type(" asd ");
+        cy.get('#editor .typo:first-of-type').click();
+        cy.get('#editor').type('{selectall}');
+        cy.get('#editor').type('{del}');
+        cy.get('div.grid1 span.text-marking.typo').contains("asd").should("not.exist");
+        cy.get('div.grid1 span.text-marking.typo').contains("gabmim").should("not.exist");
+        cy.get('div.grid1 span.text-marking.typo').contains("asd").should("not.exist");
+        cy.get('[data-test="editor"]').clear();
+    });
+});
