c73a2caa93c3cf551af07a8a61096c31cb43e78d
Andi Braimllari <>
2023-07-25 00:19:22 +0200

    changes

--- src/app/settings/settings.component.ts
+++ src/app/settings/settings.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
+import { environment } from '../../environments/environment'
+import {  } from '../local-storage/local-storage.service'
 
 @Component({
     selector: 'app-settings',
@@ -7,32 +9,39 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
     styleUrls: ['./settings.component.css']
 })
 export class SettingsComponent implements OnInit {
-    results: any;
+    ALREADY_MADE_MARKING_TYPE_SELECTIONS = 'penda-has-stored-marking-type-selections';
+
+    baseURL!: string;
+    markingTypesURL!: string;
+
     markingTypes: any[] = [];
 
     constructor(private http: HttpClient) {}
 
     ngOnInit(): void {
-        this.getApiResponse();
+        this.initializeURLs();
+        // TODO: consider making changes to `LocalStorageService`
+        this.http.get(this.markingTypesURL).subscribe((data: any) => {
+            if (!localStorage.getItem(this.ALREADY_MADE_MARKING_TYPE_SELECTIONS)) {
+                localStorage.setItem(this.ALREADY_MADE_MARKING_TYPE_SELECTIONS, 'true');
+                this.markingTypes = Object.entries(data['marking_types']).filter((e: any) => e[1].enabled);
+                this.markingTypes.forEach((mT) => localStorage.setItem(mT[0], mT[1].enabled));
+            } else {
+                // TODO: lot of cases here in which more/less types come from the endpoint than are in local storage
+                this.markingTypes = Object.entries(data['marking_types']).filter((e: any) => e[1].enabled).map((e: any) => {
+                    e[1].enabled = (localStorage.getItem(e[0]) === 'true');
+                    return e;
+                });
+            }
+        });
     }
 
-    getApiResponse() {
-        this.http
-            .get<any>(
-                // API Link
-                '[URL]'
-            )
-            .subscribe((response) => {
-                this.results = response;
-                this.markingTypes = Object.values(this.results.marking_types);
-                for (const markingType in this.results.marking_types) {
-                    if (
-                        this.results.marking_types.hasOwnProperty(markingType)
-                    ) {
-                        this.results.marking_types[markingType].description;
-                        this.results.marking_types[markingType].enabled;
-                    }
-                }
-            });
+    onMarkingTypeSelection(markingTypeID: string, e: any): void {
+        localStorage.setItem(markingTypeID, e);
+    }
+
+    private initializeURLs(): void {
+        this.baseURL = environment.baseURL;
+        this.markingTypesURL = `${this.baseURL}/api/getMarkingTypes`;
     }
 }
