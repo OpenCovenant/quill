bc99583a8af159cbc76a63506790e7ca38f4cd4f
Anderson Ceci <andersonceci31@gmail.com>
2023-07-28 00:19:15 +0200

    Made the 2 final tests and also added data-test and also runed prettier

--- cypress/e2e/disabling-a-marking-persisted-after-reloading.cy.js
+++ cypress/e2e/disabling-a-marking-persisted-after-reloading.cy.js
@@ -4,17 +4,16 @@ describe("Enabling/disabling a marking is persisted after reloading.", () => {
     });
 
     it("will uncheck all the switch on the /settings page reload the page and check if they are still disabled ", () => {
-        // Verify the button has the class 'ng-touched' indicating it's enabled
-        cy.get('[data-test="switch-check-testing"]').should('have.class', 'ng-touched');
-        cy.get('[data-test="switch-check-testing"]').click();
-        // Verify the button has the class 'ng-untouched' indicating it's disabled
         cy.get('[data-test="switch-check-testing"]').should('have.class', 'ng-untouched');
+        cy.get('[data-test="switch-check-testing"]').click({ multiple: true, force:true });
+        cy.get('[data-test="switch-check-testing"]').should('have.class', 'ng-touched');
         cy.reload();
-        cy.get('[data-test="switch-check-testing"]').should('not.be.checked');
-        cy.get('[data-test="switch-check-testing"]').click({ multiple: true });
-        cy.get('[data-test="switch-check-testing"]').should('be.checked');
+        cy.get('[data-test="switch-check-testing"]').should('have.class', 'ng-untouched');
+        cy.get('[data-test="switch-check-testing"]').click({ multiple: true, force:true });
+        cy.get('[data-test="switch-check-testing"]').should('have.class', 'ng-touched');
         cy.reload();
-        cy.get('[data-test="switch-check-testing"]').should('be.checked');
+        cy.get('[data-test="switch-check-testing"]').should('have.class', 'ng-untouched');
 
     });
 });
+
---
+++ cypress/e2e/marking-does-not-appear-after-disabling.cy.js
@@ -0,0 +1,86 @@
+describe("A marking type does not appear after being disabled.", () => {
+    beforeEach(() => {
+        cy.visit("/settings");
+    });
+
+    it("will disable all the marking types and check if they appeared on the main page", () => {
+        cy.get('[data-test="switch-check-testing"]').should('have.class', 'ng-untouched');
+        cy.get('[data-test="switch-check-testing"]').click({ multiple: true, force:true });
+        cy.get('[data-test="switch-check-testing"]').should('have.class', 'ng-touched');
+            cy.visit("/");
+            cy.get('[data-test="editor"]').type("Pra shkoi tek zyra. ");
+        cy.get("span.typo").contains("gabime shkrimi").should("exist");
+        cy.get(".list-group-item b")
+            .contains("shkrim pa gabime")
+            .should("exist");
+        cy.get(".list-group-item span")
+            .contains(
+                "shenjime për fjalë të shkruara gabim, gabime fonetikore, shenja pikësimi"
+            )
+            .should("exist");
+        //cheks for sugjerime huazimesh default marking
+        cy.get("span.loanword").contains("sugjerime huazimesh").should("exist");
+        cy.get(".list-group-item b")
+            .contains("pa huazime për tani")
+            .should("exist");
+        cy.get(".list-group-item span")
+            .contains(
+                "shenjime për huazime me origjinë italiane, angleze, sllave, gjermane, otomane, greke"
+            )
+            .should("exist");
+        //cheks for sugjerime huazimesh default marking
+        cy.get("span.stylistic")
+            .contains("shenjime stilistike")
+            .should("exist");
+        cy.get(".list-group-item b")
+            .contains("stilistikë e duhur")
+            .should("exist");
+        cy.get(".list-group-item span")
+            .contains("shenjime për fjali tepër të gjata")
+            .should("exist");
+        cy.get('[data-test="editor"]').type('{enter}');
+        cy.get('[data-test="editor"]').type("Pra  kaq.");
+        cy.get("span.typo").contains("gabime shkrimi").should("exist");
+        cy.get(".list-group-item b")
+            .contains("shkrim pa gabime")
+            .should("exist");
+        cy.get(".list-group-item span")
+            .contains(
+                "shenjime për fjalë të shkruara gabim, gabime fonetikore, shenja pikësimi"
+            )
+            .should("exist");
+        //cheks for sugjerime huazimesh default marking
+        cy.get("span.loanword").contains("sugjerime huazimesh").should("exist");
+        cy.get(".list-group-item b")
+            .contains("pa huazime për tani")
+            .should("exist");
+        cy.get(".list-group-item span")
+            .contains(
+                "shenjime për huazime me origjinë italiane, angleze, sllave, gjermane, otomane, greke"
+            )
+            .should("exist");
+        //cheks for sugjerime huazimesh default marking
+        cy.get("span.stylistic")
+            .contains("shenjime stilistike")
+            .should("exist");
+        cy.get(".list-group-item b")
+            .contains("stilistikë e duhur")
+            .should("exist");
+        cy.get(".list-group-item span")
+            .contains("shenjime për fjali tepër të gjata")
+            .should("exist");
+        cy.get('[data-test="editor"]').clear();
+        cy.visit("/settings");
+        cy.get('[data-test="switch-check-testing"]').should('have.class', 'ng-untouched');
+        cy.get('[data-test="switch-check-testing"]').click({ multiple: true, force:true });
+        cy.get('[data-test="switch-check-testing"]').should('have.class', 'ng-touched');
+            cy.visit("/");
+            cy.get('[data-test="editor"]').type("Pra shkoi tek zyra.");
+            cy.get('[data-test="text-marking-typo-test"]').contains("tek").should("exist");
+            cy.get('[data-test="editor"]').type('{enter}');
+            cy.get('[data-test="editor"]').type("Kishte buke, sallam, etj.");
+            cy.get('[data-test="text-marking-typo-test"]').contains(",").should("exist");
+
+    });
+});
+
