4479110383431e313fb9d2eacc69df1f70eea113
Andi Braimllari <>
2023-07-27 23:29:39 +0200

    add wait

--- cypress/e2e/remove-highlighted-marking.cy.js
+++ cypress/e2e/remove-highlighted-marking.cy.js
@@ -8,6 +8,7 @@ describe("Clicking on the remove-highlighted-marking actually removes the highli
         cy.get('[data-test="editor"]').type("asd ");
         cy.get('[data-test="editor"]').type(" gabmim ");
         cy.get('[data-test="editor"]').type(" asd ");
+        cy.wait(3000);
         cy.get('#editor > p > .typo').first().click();
         cy.get('[data-test="text-marking-typo-test-marked"]').contains("asd").should("exist");
         cy.get('[data-test="dismiss-marking-button"]').click();
