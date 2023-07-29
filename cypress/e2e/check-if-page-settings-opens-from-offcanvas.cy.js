27d21eddc33e3fe53e5e26ae58a18912622cb86a
Anderson Ceci <andersonceci31@gmail.com>
2023-07-30 00:02:23 +0200

    Removed unused code and finished the additional test

--- cypress/e2e/check-if-page-settings-opens-from-offcanvas.cy.js
+++ cypress/e2e/check-if-page-settings-opens-from-offcanvas.cy.js
@@ -1,11 +1,11 @@
-describe("Settings page should open from navigation page", () => {
+describe("Settings page should open from offcanvas page", () => {
     beforeEach(() => {
         cy.visit("/");
     });
 
     it("will click on the \"cilesimet\" located on offcanvas and check if it loaded the /settings page ", () => {
         cy.get('[data-test="navbar-toggler-icon"]').click();
-        cy.get('[data-test="marking-information-icon"]').click();
-        cy.url().should("include", "/te-dhe-tek");
+        cy.get('[data-test="cilesimet-button"]').click();
+        cy.url().should("include", "/settings");
     });
 });
