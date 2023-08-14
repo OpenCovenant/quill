62f3ef3b8c38c17f07d28f452a799704e64cac8b
Anderson Ceci <andersonceci31@gmail.com>
2023-08-14 23:42:51 +0200

    fixed a typo and the merge accident

--- cypress/e2e/dark_mode_button_functionality_test.cy.js
+++ cypress/e2e/dark_mode_button_functionality_test.cy.js
@@ -6,7 +6,7 @@ describe("Enable/disable dark mode button and reload to check if its working", (
     it("will enable the dark mode button reload and then disable it and reload and check whether it stays as desired", () => {
         cy.get('[data-test="navbar-toggler-icon"]').click();
         cy.get('[data-test="dark-mode-button-toggle-switch"]').should("not.be.checked");
-        cy.get('[data-test="dark-mode-button-toggle-swotch"]').click({force: true});
+        cy.get('[data-test="dark-mode-button-toggle-switch"]').click({force: true});
         cy.get('[data-test="dark-mode-button-toggle-switch"]').should("be.checked");
         cy.reload();
         cy.get('[data-test="navbar-toggler-icon"]').click();
