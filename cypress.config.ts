7b5bf785fea38b51663e3463754abd98bb7de391
Andi Braimllari <>
2023-08-15 22:46:28 +0200

    undo change

--- cypress.config.ts
+++ cypress.config.ts
@@ -5,7 +5,7 @@ export default defineConfig({
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
-    baseUrl: "http://127.0.0.1:4200"
+    baseUrl: "http://localhost:4200"
   },
     blockHosts: ["*.google-analytics.com"]
 });
