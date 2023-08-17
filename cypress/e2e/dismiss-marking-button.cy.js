29df907e488f52bf5d90361f5ab1a97a5859c41f
Andi Braimllari <>
2023-08-17 22:20:07 +0200

    changes for two buttons in markings of RHS, refactor

--- cypress/e2e/dismiss-marking-button.cy.js
+++ cypress/e2e/dismiss-marking-button.cy.js
@@ -1,33 +1,24 @@
-describe("test for `HIQ` button", () => {
-    let labels;
-
+describe("test for dismiss-marking button", () => {
     beforeEach(() => {
         cy.visit("/");
-        cy.fixture("labels").then((data) => {
-            labels = data;
-        });
     });
 
-    it("will click on `HIQ` button and check if it deletes the text on editor", () => {
+    it("will click on dismiss-marking button and check if it deletes the text on editor", () => {
         cy.get("#editor > p > .typo").should("not.exist");
         cy.get('[data-test="editor"]').type("gabmim ");
-        cy.get('[data-test="dismiss-marking-button"]')
-            .contains(labels.dismissMarkingLabel)
-            .click();
+        cy.get('[data-test="dismiss-marking-button"]').click();
         cy.get('[data-test="suggestion"]').should("not.exist");
     });
 
-    it("will click on `HIQ` buttons and check if it deletes the markings in the editor", () => {
+    it("will click on dismiss-marking buttons and check if it deletes the markings in the editor", () => {
         const text = "gabmim gabmim";
         cy.get("#editor > p > .typo").should("not.exist");
         cy.get('[data-test="editor"]').type(text);
         cy.get('[data-test="dismiss-marking-button"]')
             .first()
-            .contains(labels.dismissMarkingLabel)
             .click();
         cy.get('[data-test="dismiss-marking-button"]')
             .first()
-            .contains(labels.dismissMarkingLabel)
             .click();
         cy.get('[data-test="suggestion"]').should("not.exist");
         cy.get('[data-test="editor"]').should("have.text", text);
