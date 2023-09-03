95f6033758cbbc2ff9f1f05afde49da8669d8b19
Anderson Ceci <andersonceci31@gmail.com>
2023-09-03 23:05:55 +0200

    fixed test

--- cypress/e2e/empty-text-doesnt-save-on-history.cy.js
+++ cypress/e2e/empty-text-doesnt-save-on-history.cy.js
@@ -5,7 +5,7 @@ describe("empty text should not be on the history tab", () => {
 
     it("will type empty text and new line and should not be saved on history tab", () => {
         cy.get("#editor > p > .typo").should("not.exist");
-        cy.get('[data-test="editor"]').type("&nbsp;");
+        cy.get('[data-test="editor"]').type(" ");
         cy.wait(16000);
         cy.get('[data-test="written-texts-history-button"]').click();
         cy.get('[data-test="written-text"]').should("not.exist");
