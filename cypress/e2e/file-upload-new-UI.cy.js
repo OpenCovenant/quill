856be9340b52879252a76fc241bb3e683102dce5
Kledis <kledis.myrtollari1@gmail.com>
2023-09-19 23:48:36 +0200

    Cypress test for uploaded file in editor

---
+++ cypress/e2e/file-upload-new-UI.cy.js
@@ -0,0 +1,35 @@
+describe("tests for uploaded files shown on editor", () => {
+    let labels;
+
+    beforeEach(() => {
+        cy.visit("/");
+    });
+
+    it("will click on editor placeholder to upload a file in WORD, and will check if there is text shown in editor ", () => {
+       
+        cy.get('[data-test="editor-placeholder-upload"]')
+            .click()
+            .selectFile("cypress/fixtures/test.docx");
+        cy.get('[data-test="editor"]').contains("lider asd").should("exist");
+        
+    });
+
+
+    it("will click on editor placeholder to upload a file in PDF, and will check if there is text shown in editor", () => {
+   
+        cy.get('[data-test="editor-placeholder-upload"]')
+            .click()
+            .selectFile("cypress/fixtures/test.pdf");
+        cy.get('[data-test="editor"]').contains("lider asd").should("exist");
+    });
+
+
+    it("will click on editor placeholder to upload a file in LIBRA, and will check if there is text shown in editor", () => {
+      
+        cy.get('[data-test="editor-placeholder-upload"]')
+            .click()
+            .selectFile("cypress/fixtures/test.odt");
+        cy.get('[data-test="editor"]').contains("lider asd").should("exist");
+    });
+
+});
