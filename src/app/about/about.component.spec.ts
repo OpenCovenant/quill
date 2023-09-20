276afe4e8c31cf9756a3de71b99fc4b9cad37b20
Andi Braimllari <>
2023-09-20 21:36:54 +0200

    prettier

--- src/app/about/about.component.spec.ts
+++ src/app/about/about.component.spec.ts
@@ -1,7 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { AboutComponent } from './about.component';
-import { HttpClientTestingModule } from '@angular/common/http/testing'
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 
 describe('AboutComponent', () => {
     let component: AboutComponent;
