959d574ef67c38c123bb816698336d8502665c45
Andi Braimllari <>
2023-09-20 21:05:22 +0200

    add about

---
+++ src/app/about/about.component.ts
@@ -0,0 +1,28 @@
+import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http'
+import { environment } from '../../environments/environment'
+
+@Component({
+  selector: 'app-about',
+  templateUrl: './about.component.html',
+  styleUrls: ['./about.component.css']
+})
+export class AboutComponent {
+    baseURL!: string;
+    getAboutURL!: string;
+
+    constructor(private http: HttpClient) {
+        this.initializeURLs();
+        this.http
+            .get(this.getAboutURL)
+            .subscribe(
+                (html: any) =>
+                    (document.getElementById('about')!.innerHTML = html['about_html'])
+            );
+    }
+
+    initializeURLs() {
+        this.baseURL = environment.baseURL;
+        this.getAboutURL = this.baseURL + '/api/getAbout';
+    }
+}
