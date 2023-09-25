bc74574bc1e9eb90b3d09343e8a7d6fcd6250ccb
Andi Braimllari <>
2023-09-26 00:37:48 +0200

    add white color for marking details on dark mode

--- src/app/marking-details/marking-details.component.ts
+++ src/app/marking-details/marking-details.component.ts
@@ -3,6 +3,7 @@ import { HttpClient } from '@angular/common/http';
 import { ActivatedRoute } from '@angular/router';
 
 import { environment } from '../../environments/environment';
+import { DarkModeService } from '../dark-mode.service'
 
 @Component({
     selector: 'app-marking-details',
@@ -13,7 +14,7 @@ export class MarkingDetailsComponent {
     baseURL!: string;
     getMarkingDetailsURL!: string;
 
-    constructor(private route: ActivatedRoute, private http: HttpClient) {
+    constructor(private route: ActivatedRoute, private http: HttpClient, public darkModeService: DarkModeService) {
         this.initializeURLs();
         this.route.paramMap.subscribe((paramMap) => {
             const id = paramMap.get('id');
