4cbbdcfea9ee05587399ca3768f16c9ab5c549cd
Kledis Myrtollari <kledis.myrtollari1@gmail.com>
2023-10-12 20:38:16 +0200

    skeleton/placeholder added

--- src/app/settings/settings.component.ts
+++ src/app/settings/settings.component.ts
@@ -14,6 +14,7 @@ export class SettingsComponent implements OnInit {
 
     baseURL!: string;
     markingTypesURL!: string;
+    isLoading: boolean = true;
 
     markingTypes: any[] = [];
 
@@ -47,6 +48,7 @@ export class SettingsComponent implements OnInit {
                         e[1].enabled = localStorage.getItem(e[0]) === 'true';
                         return e;
                     });
+                this.isLoading = false;
             }
         });
     }
