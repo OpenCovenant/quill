2464f306a7f02afb5b300a71d30f9d42ebd5a3b8
Anderson Ceci <andersonceci31@gmail.com>
2023-10-21 19:30:02 +0200

    rolled back the sticky test

--- cypress/e2e/selected-marking-is-sticky.cy.js
+++ cypress/e2e/selected-marking-is-sticky.cy.js
@@ -6,21 +6,14 @@ describe("The selected marking is sticky.", () => {
     it("will add multiple new lines and check if the selected marking is still on the screen", () => {
         cy.get("#editor > p > .typo").should("not.exist");
         cy.get('[data-test="editor"]').type("gabmim");
+
         for (let i = 0; i < 30; ++i) {
             cy.get("#editor").type("{enter}");
         }
-        cy.scrollTo("top");
-        cy.get('[data-test="text-marking-span"]').should("be.visible");
+
         cy.get("#editor > p > .typo").first().click();
-        cy.get('[data-test="characters-words-markings"]').scrollIntoView();
+        cy.scrollTo("bottom");
         cy.get('[data-test="highlighted-text-marking"]').should("be.visible");
-        cy.get('[data-test="text-marking-span"]').should(($span) => {
-            // Check if the highlighted element is visible
-            if ($span.length > 0) {
-                expect($span).to.not.be.visible;
-                expect($span).not.to.exist;
-            }
-        });
         cy.get('[data-test="editor"]').clear();
     });
 });
