c66f44a5913ae21c9a22646c5778ca1dfac23192
Andi Braimllari <>
2023-11-16 23:15:22 +0100

    15k to 5k

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -42,7 +42,7 @@ export class HomeComponent implements AfterViewInit, OnDestroy {
     EMPTY_STRING: string = '';
     EDITOR_KEY: string = 'editor';
     PLACEHOLDER_ELEMENT_ID: string = 'editor-placeholder';
-    MAX_EDITOR_CHARACTERS: number = 15000;
+    MAX_EDITOR_CHARACTERS: number = 5000;
     MAX_EDITOR_CHARACTERS_MESSAGE =
         'Keni arritur kufirin e 15 mijë karaktereve, shkurtoni shkrimin';
     LINE_BREAK: string = '<br>';
