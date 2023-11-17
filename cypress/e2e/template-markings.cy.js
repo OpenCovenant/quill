1d6317be29b83970d802f1212e3b3dfa680531f3
Andi Braimllari <>
2023-11-17 21:19:33 +0100

    update and refactor tests

--- cypress/e2e/template-markings.cy.js
+++ cypress/e2e/template-markings.cy.js
@@ -1,10 +1,9 @@
-describe("will check the default markings are show when home page is loaded", () => {
+describe("will perform checks over the default markings", () => {
     beforeEach(() => {
         cy.visit("/");
     });
 
-    it("will check the default markings are show when home page is loaded", () => {
-        //checks for gabime shkrimi default marking
+    it("will check whether the default markings are shown when the home page is loaded", () => {
         cy.get("span.typo").contains("gabime shkrimi").should("exist");
         cy.get(".list-group-item b")
             .contains("shkrim pa gabime")
@@ -14,7 +13,7 @@ describe("will check the default markings are show when home page is loaded", ()
                 "shenjime për fjalë të shkruara gabim, gabime fonetikore, shenja pikësimi"
             )
             .should("exist");
-        //cheks for sugjerime huazimesh default marking
+
         cy.get("span.loanword").contains("sugjerime huazimesh").should("exist");
         cy.get(".list-group-item b")
             .contains("pa huazime për tani")
@@ -24,7 +23,7 @@ describe("will check the default markings are show when home page is loaded", ()
                 "shenjime për huazime me origjinë italiane, angleze, sllave, gjermane, otomane, greke"
             )
             .should("exist");
-        //cheks for sugjerime huazimesh default marking
+
         cy.get("span.stylistic")
             .contains("shenjime stilistike")
             .should("exist");
@@ -33,6 +32,40 @@ describe("will check the default markings are show when home page is loaded", ()
             .should("exist");
         cy.get(".list-group-item span")
             .contains("shenjime për fjali tepër të gjata")
+            .should("exist");
+    });
+
+    it("will check whether the default markings are shown when a suggestion is applied", () => {
+        cy.get("span.typo").contains("gabime shkrimi").should("exist");
+        cy.get("span.loanword").contains("sugjerime huazimesh").should("exist");
+        cy.get("span.stylistic")
+            .contains("shenjime stilistike")
+            .should("exist");
+
+        cy.get('[data-test="editor"]').type("sakt eshte");
+        cy.get('[data-test="suggestion"]').contains("është").click();
+
+        cy.get("span.typo").contains("gabime shkrimi").should("exist");
+        cy.get("span.loanword").contains("sugjerime huazimesh").should("exist");
+        cy.get("span.stylistic")
+            .contains("shenjime stilistike")
+            .should("exist");
+    });
+
+    it("will check whether the default markings are shown when a marking is dismissed", () => {
+        cy.get("span.typo").contains("gabime shkrimi").should("exist");
+        cy.get("span.loanword").contains("sugjerime huazimesh").should("exist");
+        cy.get("span.stylistic")
+            .contains("shenjime stilistike")
+            .should("exist");
+
+        cy.get('[data-test="editor"]').type("sakt eshte");
+        cy.get('[data-test="dismiss-marking-button"]').click();
+
+        cy.get("span.typo").contains("gabime shkrimi").should("exist");
+        cy.get("span.loanword").contains("sugjerime huazimesh").should("exist");
+        cy.get("span.stylistic")
+            .contains("shenjime stilistike")
             .should("exist");
     });
 });
