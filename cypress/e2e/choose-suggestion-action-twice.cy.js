3d90144b696c32ff9ceddc79d431e806f91e80f5
AndiBraimllari <AndiBraimllari@users.noreply.github.com>
2023-11-28 17:37:46 +0100

    update a cypress test (#374)
    Co-authored-by: Andi Braimllari <>

--- cypress/e2e/choose-suggestion-action-twice.cy.js
+++ cypress/e2e/choose-suggestion-action-twice.cy.js
@@ -1,36 +1,22 @@
-describe("", () => {
+describe("the highlight of markings intertwined with some other marking operations", () => {
     beforeEach(() => {
         cy.visit("/");
     });
 
-    it("", () => {
-        // Check initial state
+    it("will behave as expected as we perform different operations on the markings", () => {
         cy.get("#editor > p > .typo").should("not.exist");
-
-        // Type some text
         cy.get('[data-test="editor"]').type("asd gabmim asd ");
-
-        // Wait for the button to appear
         cy.get("#editor > p > .typo").should("be.visible");
 
         cy.get("#editor > p > .typo").first().click()
-
-        cy.get(".card").find('[data-test="blur-marking-button"]').click();
-
+        cy.get('[data-test="marking-card"]').find('[data-test="blur-marking-button"]').click();
         cy.get("#editor > p > .typo").first().click()
 
-        cy.get(".card").find('[data-test="suggestion"]').first().click();
-
+        cy.get('[data-test="marking-card"]').find('[data-test="suggestion"]').first().click();
         cy.get("#editor > p > .typo").first().click()
-
-        cy.get(".card").find('[data-test="suggestion"]').first().click();
-
-
-        // Click the button for each card
-        // cy.get(".sticky .card").each((card, index) => {
-        //     cy.wrap(card).find('[data-test="dismiss-marking-button"]').click();
-        //
-        //     cy.wrap(card).should("not.exist");
-        // });
+        cy.get('[data-test="marking-card"]').find('[data-test="suggestion"]').first().click();
+        cy.get('[data-test="marking-card"]').find('[data-test="dismiss-marking-button"]').click();
+        cy.get("#editor > p > .typo").should("not.exist");
+        cy.get('[data-test="marking-card"]').should("not.exist");
     });
 });
