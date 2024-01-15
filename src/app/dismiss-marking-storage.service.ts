9c3702c9f9098850bfa6a7a28808edc87f46cbfa
Anderson Ceci <andersonceci31@gmail.com>
2024-01-15 23:43:21 +0100

    fixed some variables

--- src/app/dismiss-marking-storage.service.ts
+++ src/app/dismiss-marking-storage.service.ts
@@ -8,7 +8,7 @@ export class DismissMarkingStorageService {
     DISMISSED_MARKING_PREFIX = 'penda-dismissed-markings-';
     canStoreDismissedMarking: boolean = true;
     EMPTY_STRING: string = '';
-    WRITTEN_TEXTS_KEYS: string[] = [];
+    DISMISSED_TEXTS_KEYS: string[] = [];
 
     constructor() {
         const readyToStoreDismissedMarkings = localStorage.getItem(this.DISMISSED_MARKING_KEY);
@@ -21,14 +21,14 @@ export class DismissMarkingStorageService {
             this.canStoreDismissedMarking = false;
         }
 
-        this.generateWrittenTextsKeys();
+        this.generateDismissedTextKeys();
     }
 
-    generateWrittenTextsKeys(): void {
+    generateDismissedTextKeys(): void {
         if (this.canStoreDismissedMarking) {
             const numberOfKeysToGenerate = 5;
 
-            this.WRITTEN_TEXTS_KEYS = Array(numberOfKeysToGenerate)
+            this.DISMISSED_TEXTS_KEYS = Array(numberOfKeysToGenerate)
                 .fill(this.DISMISSED_MARKING_PREFIX)
                 .map((prefix: string, i: number) => prefix + i.toString());
         }
@@ -40,31 +40,31 @@ export class DismissMarkingStorageService {
         }
 
         const arr: string[] = [];
-        for (let i = 0; i < this.WRITTEN_TEXTS_KEYS.length; i++) {
-            arr.push(localStorage.getItem(this.WRITTEN_TEXTS_KEYS[i])!);
+        for (let i = 0; i < this.DISMISSED_TEXTS_KEYS.length; i++) {
+            arr.push(localStorage.getItem(this.DISMISSED_TEXTS_KEYS[i])!);
             if (!arr[arr.length - 1]) {
                 for (let j = i; j >= 0; j--) {
                     if (j === 0) {
                         localStorage.setItem(
-                            this.WRITTEN_TEXTS_KEYS[j],
+                            this.DISMISSED_TEXTS_KEYS[j],
                             dismissedText
                         );
                     } else {
                         localStorage.setItem(
-                            this.WRITTEN_TEXTS_KEYS[j],
+                            this.DISMISSED_TEXTS_KEYS[j],
                             arr[j - 1]
-                        );
+                        )
                     }
                 }
                 return;
             }
         }
 
-        for (let i = this.WRITTEN_TEXTS_KEYS.length - 1; i >= 0; i--) {
+        for (let i = this.DISMISSED_TEXTS_KEYS.length - 1; i >= 0; i--) {
             if (i === 0) {
-                localStorage.setItem(this.WRITTEN_TEXTS_KEYS[i], dismissedText);
+                localStorage.setItem(this.DISMISSED_TEXTS_KEYS[i], dismissedText);
             } else {
-                localStorage.setItem(this.WRITTEN_TEXTS_KEYS[i], arr[i - 1]);
+                localStorage.setItem(this.DISMISSED_TEXTS_KEYS[i], arr[i - 1]);
             }
         }
     }
