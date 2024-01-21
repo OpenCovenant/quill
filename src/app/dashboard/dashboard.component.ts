288b5665baac1ad693d79523a12265aae779c9a6
Andi Braimllari <>
2024-01-21 15:41:22 +0100

    changes

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { DarkModeService } from '../dark-mode.service'
+import { HttpClient } from '@angular/common/http'
+import { environment } from '../../environments/environment'
 
 @Component({
   selector: 'app-dashboard',
@@ -6,4 +9,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./dashboard.component.css']
 })
 export class DashboardComponent {
+    baseURL!: string;
+    getDashboardURL!: string;
+
+    constructor(
+        private http: HttpClient,
+        public darkModeService: DarkModeService
+    ) {
+        this.initializeURLs();
+        this.http
+            .get(this.getDashboardURL)
+            .subscribe(
+                (html: any) =>
+                    (document.getElementById('dashboard')!.innerHTML =
+                        html['dashboard_html'])
+            );
+    }
+
+    initializeURLs() {
+        this.baseURL = environment.baseURL;
+        this.getDashboardURL = this.baseURL + '/api/getDashboard';
+    }
 }
