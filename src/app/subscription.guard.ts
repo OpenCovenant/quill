6191d6275516b338247552dec244ba7a5d7771f5
Andi Braimllari <>
2024-02-02 20:13:26 +0100

    changes

--- src/app/subscription.guard.ts
+++ src/app/subscription.guard.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from '@angular/router'
-import { map, Observable } from 'rxjs'
-import { AuthenticationService } from './authentication.service'
+import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { map, Observable } from 'rxjs';
+import { AuthenticationService } from './authentication.service';
 
 @Injectable({
   providedIn: 'root'
