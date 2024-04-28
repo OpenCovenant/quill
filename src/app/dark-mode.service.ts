abd705f3f8fe1ce65fdd253d8fe5e97831c1b713
Kledis Myrtollari <kledis.m@frontlevels.com>
2024-04-28 20:56:52 +0200

    fixing transition of dark mode button which slided to the left on page load

--- src/app/dark-mode.service.ts
+++ src/app/dark-mode.service.ts
@@ -27,9 +27,18 @@ export class DarkModeService {
 
     toggleDarkMode(): void {
         this.isDarkMode = !this.isDarkMode;
+        this.addTransitionClass();
+    }
+
+    addTransitionClass(): void {
+        const darkModeSliders = document.querySelectorAll('.dark-mode-slider');
+        darkModeSliders.forEach(slider => {
+            slider.classList.add('transition-enabled');
+        });
     }
 
     onDarkModeChange(): void {
+        this.addTransitionClass();
         localStorage.setItem(this.DARK_MODE, String(this.isDarkMode));
     }
 }
