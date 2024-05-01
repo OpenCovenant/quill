1422d8a0d0952fa1e2f184201bc7619fca0bf902
AndiBraimllari <AndiBraimllari@users.noreply.github.com>
2024-05-02 00:56:44 +0200

    refactor (#467)
    Co-authored-by: Andi Braimllari <>

--- cypress/e2e/focus-editor-placeholder.cy.js
+++ cypress/e2e/focus-editor-placeholder.cy.js
@@ -1,10 +1,13 @@
-xdescribe("Clicking on editor placeholder must not lose the focus on editor", () => {
+describe("Clicking on editor placeholder must not lose the focus on editor", () => {
     beforeEach(() => {
         cy.visit("/");
     });
 
-    it("Clicking on placeholder text should not lose focus on editor", () => {
-        cy.get('[data-test="editor-placeholder-text"]').click();
+    it("should retain focus on the editor when clicking on the placeholder text", () => {
+        cy.get('[data-test="editor-placeholder-text"]').then(v => {
+            // TODO: derive (40, 10) or equivalent from `v[0].getBoundingClientRect();` or whatever is needed
+            cy.get('[data-test="editor"]').click(40, 10)
+        });
         cy.get('[data-test="editor"]').should('be.focused');
     });
 });
