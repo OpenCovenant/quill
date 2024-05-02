3e7396185e8f135d9865888484793b7575d1a791
Andi Braimllari <>
2024-05-03 00:28:53 +0200

    merge three Cypress tests to one

--- cypress/e2e/file-upload.cy.js
+++ cypress/e2e/file-upload.cy.js
@@ -1,4 +1,4 @@
-describe("Upload Document Button Test", () => {
+describe("Upload Document", () => {
     let labels;
 
     beforeEach(() => {
@@ -11,62 +11,83 @@ describe("Upload Document Button Test", () => {
     // TODO: also check for updated word/character/marking information
 
     it("should check for loanword in output container on WORD file", () => {
-        // TODO update?
-        // cy.get('button[id="uploadDocumentToggleButton"]').click();
         cy.get('[data-test="editor-placeholder-upload"]')
-            // .contains(labels.uploadFileLabel)
             .click()
             .selectFile("cypress/fixtures/test.docx");
+        cy.get('[data-test="editor"]').should("have.text", "lider asd");
         cy.get(".loanword").contains("lider").should("exist");
     });
 
     it("should check for typo in output container on WORD file", () => {
-        // TODO update?
-        // cy.get('button[id="uploadDocumentToggleButton"]').click();
         cy.get('[data-test="editor-placeholder-upload"]')
-            // .contains(labels.uploadFileLabel)
             .click()
             .selectFile("cypress/fixtures/test.docx");
+        cy.get('[data-test="editor"]').should("have.text", "lider asd");
         cy.get(".typo").contains("asd").should("exist");
     });
 
+    it("should upload the same WORD file 3 times in a row", () => {
+        for (let n = 0; n < 3; n++) {
+            cy.get('[data-test="editor-placeholder-upload"]')
+                .click()
+                .selectFile("cypress/fixtures/test.docx");
+            cy.get('[data-test="editor"]').should("have.text", "lider asd");
+            cy.get('[data-test="editor"]').clear();
+        }
+    });
+
     it("should check for loanword in output container on PDF file", () => {
-        // TODO update?
-        // cy.get('button[id="uploadDocumentToggleButton"]').click();
         cy.get('[data-test="editor-placeholder-upload"]')
-            // .contains(labels.uploadFileLabel)
             .click()
             .selectFile("cypress/fixtures/test.pdf");
+        cy.get('[data-test="editor"]').should("have.text", "lider asd");
         cy.get(".loanword").contains("lider").should("exist");
     });
 
     it("should check for typo in output container on PDF file", () => {
-        // TODO update?
-        // cy.get('button[id="uploadDocumentToggleButton"]').click();
         cy.get('[data-test="editor-placeholder-upload"]')
-            // .contains(labels.uploadFileLabel)
             .click()
             .selectFile("cypress/fixtures/test.pdf");
+        cy.get('[data-test="editor"]').should("have.text", "lider asd");
         cy.get(".typo").contains("asd").should("exist");
     });
 
+    it("should upload the same PDF file 3 times in a row", () => {
+        for (let n = 0; n < 3; n++) {
+            cy.get('[data-test="editor-placeholder-upload"]')
+                .click()
+                .selectFile("cypress/fixtures/test.pdf");
+            cy.get('[data-test="editor"]')
+                .contains("lider asd")
+                .should("exist");
+            cy.get('[data-test="editor"]').clear();
+        }
+    });
+
     it("should check for loanword in output container on LIBRA file", () => {
-        // TODO update?
-        // cy.get('button[id="uploadDocumentToggleButton"]').click();
         cy.get('[data-test="editor-placeholder-upload"]')
-            // .contains(labels.uploadFileLabel)
             .click()
             .selectFile("cypress/fixtures/test.odt");
+        cy.get('[data-test="editor"]').should("have.text", "lider asd");
         cy.get(".loanword").contains("lider").should("exist");
     });
 
     it("should check for typo in output container on LIBRA file", () => {
-        // TODO update?
-        // cy.get('button[id="uploadDocumentToggleButton"]').click();
         cy.get('[data-test="editor-placeholder-upload"]')
             // .contains(labels.uploadFileLabel)
             .click()
             .selectFile("cypress/fixtures/test.odt");
+        cy.get('[data-test="editor"]').should("have.text", "lider asd");
         cy.get(".typo").contains("asd").should("exist");
+    });
+
+    it("should upload the same LIBRA file 3 times in a row", () => {
+        for (let n = 0; n < 3; n++) {
+            cy.get('[data-test="editor-placeholder-upload"]')
+                .click()
+                .selectFile("cypress/fixtures/test.odt");
+            cy.get('[data-test="editor"]').should("have.text", "lider asd");
+            cy.get('[data-test="editor"]').clear();
+        }
     });
 });
