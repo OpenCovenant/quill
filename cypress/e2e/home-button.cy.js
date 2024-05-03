c03d88021dd258c9cfe5ee4fa2cf61f1c4094561
Andi Braimllari <>
2024-05-04 00:16:38 +0200

    changes

--- cypress/e2e/home-button.cy.js
+++ cypress/e2e/home-button.cy.js
@@ -1,10 +1,10 @@
-describe("Home Button Test", () => {
+describe("Home Button", () => {
     beforeEach(() => {
         cy.visit("/te-dhe-tek");
     });
 
     it("should click on the PENDA home button, redirecting to the home page", () => {
-        cy.get('[data-test="penda-home-button"]').click();
+        cy.get('[data-test="home-button"]').click();
         cy.url().should("include", "");
     });
 });
--- cypress/e2e/maintain-editor-content.cy.js
+++ cypress/e2e/maintain-editor-content.cy.js
@@ -8,9 +8,9 @@ describe("Written Text Should Stay On Editor When Navigating On Other Pages", ()
         cy.get('[data-test="editor"]').type("gabmim ");
         cy.get('[data-test="gear-button-testing"]').click();
         cy.url().should("include", "/settings");
-        cy.get('[data-test="penda-home-button"]').click();
+        cy.get('[data-test="home-button"]').click();
         cy.url().should("include", "");
-        cy.get('[data-test="editor"]').contains("gabmim").should("exist");
-        cy.get('[data-test="marking-span"').contains("gabmim").should("exist");
+        cy.get('[data-test="editor"]').contains("gabmim").should("be.visible");
+        cy.get('[data-test="marking-span"').contains("gabmim").should("be.visible");
     });
 });
