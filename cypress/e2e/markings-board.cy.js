62653551cf2b935491e5a4af79acd3187d26ee3e
Andi Braimllari <>
2024-05-05 11:55:12 +0200

    changes

--- cypress/e2e/markings-board.cy.js
+++ cypress/e2e/markings-board.cy.js
@@ -3,6 +3,9 @@ describe("markings board", () => {
         cy.visit("/");
     });
 
-    it("should ...", () => {
+    it("should navigate to the detailed information page when the information button of a marking is clicked", () => {
+        cy.get('[data-test="editor"]').type("Shkoi tek zyra.");
+        cy.get('[data-test="marking-information-icon"]').click();
+        cy.url().should("include", "/te-dhe-tek");
     });
 });
