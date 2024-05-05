c58f2db968f143df813586248ed3851aa1b0befd
Andi Braimllari <>
2024-05-05 10:40:09 +0200

    changes

--- cypress/e2e/select-all-text-and-delete-removes-highlight.cy.js
+++ cypress/e2e/select-all-text-and-delete-removes-highlight.cy.js
@@ -4,12 +4,10 @@ describe("Remove Highlighted Marking by Deleting Selected Text", () => {
     });
 
     it("should remove the highlighted marking when selecting all text and deleting it", () => {
-        cy.get(".typo").should("not.exist");
         cy.get('[data-test="editor"]').type("asd gabmim asd ");
         cy.get(".typo").first().click();
         cy.get('[data-test="editor"]').type("{selectall}");
         cy.get('[data-test="editor"]').type("{del}");
         cy.get('[data-test="highlighted-marking"]').should("not.exist");
-        cy.get('[data-test="editor"]').clear();
     });
 });
