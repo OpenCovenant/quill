8680ad67d7743727f189259fe95bfcef023f8d5e
Andi Braimllari <>
2024-05-05 14:35:45 +0200

    changes

--- cypress/e2e/marking-animations.cy.js
+++ cypress/e2e/marking-animations.cy.js
@@ -1,6 +1,16 @@
-describe("Move Up Marking Cards Animation", () => {
+describe("Suggestion Marking - Fade Right Animation", () => {
     beforeEach(() => {
         cy.visit("/");
+    });
+
+    it("should replace all markings with the first suggestions and trigger fade animation", () => {
+        cy.get('[data-test="editor"]').type("gabmim gabmim");
+        cy.get('[data-test="suggestion"]').should("be.visible");
+
+        cy.get('[data-test="marking-card"]').each((card, index) => {
+            cy.wrap(card).find('[data-test="suggestion"]').first().click();
+        }); // TODO: I guess ideally we also check that this fade-out class is applied briefly?
+        cy.get('[data-test="marking-card"] .fade-out').should("not.exist");
     });
 
     it("should trigger move-up animation for remaining cards after deleting one card", () => {
