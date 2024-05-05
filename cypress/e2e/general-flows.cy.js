e242ee3cbfa47b1e8713b9d8494be56587ba46bc
Andi Braimllari <>
2024-05-05 22:13:19 +0200

    changes

--- cypress/e2e/general-flows.cy.js
+++ cypress/e2e/general-flows.cy.js
@@ -207,6 +207,20 @@ describe("general flows", () => {
         cy.get('[data-test="marking-card"]').should("not.exist");
     });
 
+    it("should remove the highlighted marking when blurring the highlighted marking", () => {
+        cy.get('[data-test="editor"]').type("asd gabmim asd ");
+        cy.get(".typo").first().click();
+        cy.get('[data-test="highlighted-marking"]')
+            .contains("asd")
+            .should("be.visible");
+
+        cy.get('[data-test="blur-marking-button"]').click();
+        cy.get('[data-test="highlighted-marking"]').should("not.exist");
+        cy.get('[data-test="marking-span"]').contains("asd").should("be.visible");
+        cy.get('[data-test="marking-span"]').contains("gabmim").should("be.visible");
+        cy.get('[data-test="marking-span"]').contains("asd").should("be.visible");
+    });
+
     it("should remove the highlighted marking when selecting all text in the editor and deleting it", () => {
         cy.get('[data-test="editor"]').type("asd gabmim asd ");
         cy.get(".typo").first().click();
