da11c2f068065379b94e0c6a4f2ab3f22c96983d
Andi Braimllari <>
2024-05-05 19:03:25 +0200

    changes

--- cypress/e2e/general-flows.cy.js
+++ cypress/e2e/general-flows.cy.js
@@ -172,4 +172,22 @@ describe("General Quill Flow", () => {
             "gabmim "
         );
     });
+
+    it("should remove the highlighted marking when typing in the editor", () => {
+        cy.get('[data-test="editor"]').type("gabmim asd ");
+        cy.get(".typo").first().click();
+        cy.get('[data-test="highlighted-marking"]').should(
+            "have.text",
+            "gabmim"
+        );
+        cy.get('[data-test="highlighted-marking"]')
+            .contains("asd")
+            .should("not.exist");
+
+        cy.get('[data-test="editor"]').type("{end}");
+        cy.get('[data-test="editor"]').type("pra ");
+        cy.get('[data-test="highlighted-marking"]').should("not.exist");
+        cy.get('[data-test="marking-span"]').contains("gabmim").should("be.visible");
+        cy.get('[data-test="marking-span"]').contains("asd").should("be.visible");
+    });
 });
--- cypress/e2e/remove-highlighted-marking.cy.js
+++ cypress/e2e/remove-highlighted-marking.cy.js
@@ -3,19 +3,17 @@ describe("Removing Highlighted Marking", () => {
         cy.visit("/");
     });
 
-    it("should remove the focused highlight when clicking on remove-highlighted-marking", () => {
-        cy.get(".typo").should("not.exist");
-        cy.get('[data-test="editor"]').type("asd ");
-        cy.get('[data-test="editor"]').type("gabmim ");
-        cy.get('[data-test="editor"]').type("asd ");
+    it("should remove the focused highlight when clicking on blurring the highlighted marking", () => {
+        cy.get('[data-test="editor"]').type("asd gabmim asd ");
         cy.get(".typo").first().click();
         cy.get('[data-test="highlighted-marking"]')
             .contains("asd")
-            .should("exist");
+            .should("be.visible");
+
         cy.get('[data-test="blur-marking-button"]').click();
-        cy.get('[data-test="marking-span"]').contains("asd").should("exist");
-        cy.get('[data-test="marking-span"]').contains("gabmim").should("exist");
-        cy.get('[data-test="marking-span"]').contains("asd").should("exist");
-        cy.get('[data-test="editor"]').clear();
+        cy.get('[data-test="highlighted-marking"]').should("not.exist");
+        cy.get('[data-test="marking-span"]').contains("asd").should("be.visible");
+        cy.get('[data-test="marking-span"]').contains("gabmim").should("be.visible");
+        cy.get('[data-test="marking-span"]').contains("asd").should("be.visible");
     });
 });
