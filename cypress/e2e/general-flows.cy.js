5f1af1f1eee30a96abc46013877226cc934a990d
Andi Braimllari <>
2024-05-05 18:39:37 +0200

    changes

--- cypress/e2e/general-flows.cy.js
+++ cypress/e2e/general-flows.cy.js
@@ -130,4 +130,17 @@ describe("General Quill Flow", () => {
                 expect(numberOfMarkings).to.equal(totalNumberOfMarkings);
             });
     });
+
+    xit("should stick the highlighted remain on the top after adding multiple new lines", () => {
+        cy.get('[data-test="editor"]').type("gabmim");
+
+        for (let i = 0; i < 30; ++i) {
+            cy.get('[data-test="editor"]').type("{enter}");
+        }
+
+        cy.get(".typo").first().click();
+        // TODO: similar effect to cy.scrollTo("bottom"); cy.wait(3000); so why does the scroll "take time"?
+        cy.get('[data-test="contact"]').dblclick();
+        cy.get('[data-test="highlighted-marking"]').should("be.visible");
+    });
 });
