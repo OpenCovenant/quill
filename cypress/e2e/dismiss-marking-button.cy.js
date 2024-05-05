1540f6d62fd94bc5c56948c4afb8563a8420b4d8
Andi Braimllari <>
2024-05-05 18:26:26 +0200

    changes

--- cypress/e2e/dismiss-marking-button.cy.js
+++ cypress/e2e/dismiss-marking-button.cy.js
@@ -3,8 +3,7 @@ describe("Dismiss Marking Button Test", () => {
         cy.visit("/");
     });
 
-    it("should delete the text in the editor when dismiss-marking button is clicked", () => {
-        cy.get(".typo").should("not.exist");
+    it("should delete the marking in the editor when the dismiss-marking button is clicked", () => {
         cy.get('[data-test="editor"]').type("gabmim ");
         cy.get('[data-test="dismiss-marking-button"]').click();
         cy.get('[data-test="suggestion"]').should("not.exist");
@@ -12,19 +11,12 @@ describe("Dismiss Marking Button Test", () => {
 
     it("should delete the markings in the editor when dismiss-marking buttons are clicked", () => {
         const text = "gabmim gabmim";
-        cy.get(".typo").should("not.exist");
         cy.get('[data-test="editor"]').type(text);
-
-        // Click on each dismiss-marking button
         cy.get('[data-test="marking-card"]').each((card) => {
             cy.wrap(card).find('[data-test="dismiss-marking-button"]').click();
-
             cy.wrap(card).should("not.exist");
         });
-
-        // Ensure that there are no remaining suggestions
-        cy.get('[data-test="suggestion"]').should("not.exist");
-        // Ensure that the editor content matches the original text
+        cy.get('[data-test="suggestion"]').should("not.be.visible");
         cy.get('[data-test="editor"]').should("have.text", text);
     });
 });
