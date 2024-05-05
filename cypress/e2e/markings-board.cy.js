a54574a8a5f34f91542e31b9856c403181c260cd
Andi Braimllari <>
2024-05-05 11:56:26 +0200

    changes

--- cypress/e2e/markings-board.cy.js
+++ cypress/e2e/markings-board.cy.js
@@ -8,4 +8,47 @@ describe("markings board", () => {
         cy.get('[data-test="marking-information-icon"]').click();
         cy.url().should("include", "/te-dhe-tek");
     });
+
+    it("should click on expand/collapse arrows and choose a suggestion", () => {
+        cy.get('[data-test="editor"]').type("sakt eshte");
+        cy.get('[data-test="suggestion"]').children().should("have.length", 8);
+        cy.get(
+            '[data-test="oscillate-suggestions-button"].bi-arrow-right-square'
+        )
+            .first()
+            .click();
+        cy.get(
+            '[data-test="oscillate-suggestions-button"].bi-arrow-right-square'
+        ).click();
+        cy.get('[data-test="suggestion"]')
+            .children()
+            .should("have.length.gt", 8);
+        cy.get(
+            '[data-test="oscillate-suggestions-button"].bi-arrow-left-square'
+        )
+            .first()
+            .click();
+        cy.get(
+            '[data-test="oscillate-suggestions-button"].bi-arrow-left-square'
+        ).click();
+        cy.get('[data-test="suggestion"]').children().should("have.length", 8);
+        cy.get(
+            '[data-test="oscillate-suggestions-button"].bi-arrow-right-square'
+        )
+            .first()
+            .click();
+        cy.get(
+            '[data-test="oscillate-suggestions-button"].bi-arrow-right-square'
+        ).click();
+        cy.get('[data-test="suggestion"]')
+            .children()
+            .should("have.length.gt", 8);
+        cy.get('[data-test="suggestion"]').contains("saktë").click();
+        cy.wait(5000);
+        cy.get(
+            '[data-test="oscillate-suggestions-button"].bi-arrow-right-square'
+        ).click();
+        cy.get('[data-test="suggestion"]').contains("është").click();
+        cy.get('[data-test="editor"]').should("have.text", "saktë është");
+    });
 });
