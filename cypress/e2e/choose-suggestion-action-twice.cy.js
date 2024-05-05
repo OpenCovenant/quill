0ca1a810fb8b0f52c550489f97c564ecadb3dbbc
Andi Braimllari <>
2024-05-05 10:28:14 +0200

    changes

--- cypress/e2e/choose-suggestion-action-twice.cy.js
+++ cypress/e2e/choose-suggestion-action-twice.cy.js
@@ -4,7 +4,6 @@ describe("Highlighting Markings Through Various Operations", () => {
     });
 
     it("should behave as expected when performing different operations on the markings", () => {
-        cy.get(".typo").should("not.exist");
         cy.get('[data-test="editor"]').type("asd gabmim asd ");
         cy.get(".typo").should("be.visible");
 
@@ -13,16 +12,21 @@ describe("Highlighting Markings Through Various Operations", () => {
             .find('[data-test="blur-marking-button"]')
             .click();
         cy.get(".typo").first().click();
+        cy.get("#editor > p > .typo").should("have.length", 3).should('be.visible');
 
         cy.get('[data-test="marking-card"]')
             .find('[data-test="suggestion"]')
             .first()
             .click();
         cy.get(".typo").first().click();
+        cy.get("#editor > p > .typo").should("have.length", 2).should('be.visible');
+
         cy.get('[data-test="marking-card"]')
             .find('[data-test="suggestion"]')
             .first()
             .click();
+        cy.get("#editor > p > .typo").should("have.length", 1).should('be.visible');
+
         cy.get('[data-test="marking-card"]')
             .find('[data-test="dismiss-marking-button"]')
             .click();
