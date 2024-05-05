92428cc27a0f17ff5e04987d6cb5de3549707050
Andi Braimllari <>
2024-05-05 10:47:39 +0200

    changes

--- cypress/e2e/check-loanwords.cy.js
+++ cypress/e2e/check-loanwords.cy.js
@@ -4,22 +4,15 @@ describe("Editor Interaction with Loanword TextField", () => {
     });
 
     it("should reflect changes in the loanword text field when suggestions are clicked in the editor", () => {
-        cy.get(".loanword").should("not.exist");
         cy.get('[data-test="editor"]').type("lider ");
         cy.get('[data-test="suggestion"]').contains("prijës").click();
-        cy.get('[data-test="editor"]').contains("prijës").should("exist");
+        cy.get('[data-test="editor"]').contains("prijës").should("be.visible");
         cy.get('[data-test="clear-editor-icon"]').click();
-
         cy.get(".loanword").should("not.exist");
-        cy.get('[data-test="editor"]').type("lider ");
-        cy.get('[data-test="suggestion"]').contains("drejtues").click();
-        cy.get('[data-test="editor"]').contains("drejtues").should("exist");
-        cy.get('[data-test="clear-editor-icon"]').click();
 
-        cy.get(".loanword").should("not.exist");
         cy.get('[data-test="editor"]').type("lider ");
         cy.get('[data-test="suggestion"]').contains("udhëheqës").click();
-        cy.get('[data-test="editor"]').contains("udhëheqës").should("exist");
+        cy.get('[data-test="editor"]').contains("udhëheqës").should("be.visible");
         cy.get('[data-test="clear-editor-icon"]').click();
     });
 });
--- cypress/e2e/check-typos.cy.js
+++ cypress/e2e/check-typos.cy.js
@@ -4,28 +4,15 @@ describe("Editor Interaction with Typo TextField", () => {
     });
 
     it("should reflect changes in the typo text field when suggestions are clicked in the editor", () => {
-        cy.get(".typo").should("not.exist");
         cy.get('[data-test="editor"]').type("gabmim ");
         cy.get('[data-test="suggestion"]').contains("gabime").click();
-        cy.get('[data-test="editor"]').contains("gabime").should("exist");
+        cy.get('[data-test="editor"]').contains("gabime").should("be.visible");
         cy.get('[data-test="editor"]').clear();
-
         cy.get(".typo").should("not.exist");
-        cy.get('[data-test="editor"]').type("gabmim ");
-        cy.get('[data-test="suggestion"]').contains("gabimi").click();
-        cy.get('[data-test="editor"]').contains("gabimi").should("exist");
-        cy.get('[data-test="editor"]').clear();
 
-        cy.get(".typo").should("not.exist");
-        cy.get('[data-test="editor"]').type("gabmim ");
-        cy.get('[data-test="suggestion"]').contains("gazmim").click();
-        cy.get('[data-test="editor"]').contains("gazmim").should("exist");
-        cy.get('[data-test="editor"]').clear();
-
-        cy.get(".typo").should("not.exist");
         cy.get('[data-test="editor"]').type("gabmim ");
         cy.get('[data-test="suggestion"]').contains("gabim").click();
-        cy.get('[data-test="editor"]').contains("gabim").should("exist");
+        cy.get('[data-test="editor"]').contains("gabim").should("be.visible");
         cy.get('[data-test="editor"]').clear();
     });
 });
