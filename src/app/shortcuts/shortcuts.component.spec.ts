1baaa9af40d6e3f175fa9845737714d2611e63fa
Andi Braimllari <>
2024-08-15 10:59:23 +0200

    refactor

--- src/app/shortcuts/shortcuts.component.spec.ts
+++ src/app/shortcuts/shortcuts.component.spec.ts
@@ -2,7 +2,7 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ShortcutsComponent } from './shortcuts.component';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
-import { RouterTestingModule } from '@angular/router/testing'
+import { RouterTestingModule } from '@angular/router/testing';
 
 describe('ShortcutsComponent', () => {
     let component: ShortcutsComponent;
