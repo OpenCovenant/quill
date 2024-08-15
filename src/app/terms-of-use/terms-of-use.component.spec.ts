4994e7a4d5f00f863011e238cdbebd25ce05eb5d
AndiBraimllari <AndiBraimllari@users.noreply.github.com>
2024-08-15 10:51:05 +0200

    fix Karma tests (#495)
    Co-authored-by: Andi Braimllari <>

--- src/app/terms-of-use/terms-of-use.component.spec.ts
+++ src/app/terms-of-use/terms-of-use.component.spec.ts
@@ -2,6 +2,7 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { TermsOfUseComponent } from './terms-of-use.component';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { RouterTestingModule } from '@angular/router/testing'
 
 describe('TermsOfUseComponent', () => {
     let component: TermsOfUseComponent;
@@ -10,7 +11,7 @@ describe('TermsOfUseComponent', () => {
     beforeEach(async () => {
         await TestBed.configureTestingModule({
             declarations: [TermsOfUseComponent],
-            imports: [HttpClientTestingModule]
+            imports: [RouterTestingModule, HttpClientTestingModule]
         }).compileComponents();
     });
 
