67c6a06615a8f9c85cf965525cfe98cfaa2fa4b2
Andi Braimllari <>
2024-09-13 21:59:19 +0200

    refactor

--- src/app/services/dark-mode.service.ts
+++ src/app/services/dark-mode.service.ts
@@ -4,9 +4,9 @@ import { Injectable } from '@angular/core';
     providedIn: 'root'
 })
 export class DarkModeService {
-    DARK_MODE_KEY = 'penda-dark-mode';
-    DARK_MODE_SYSTEM_KEY = 'penda-dark-mode-system';
-    //is dark mode kerkon te shikoje nqs eshte true ose false pra nqs color scheme is darkmode = true else false
+    private readonly DARK_MODE_KEY = 'penda-dark-mode';
+    private readonly DARK_MODE_SYSTEM_KEY = 'penda-dark-mode-system';
+
     isDarkMode: boolean = window.matchMedia('(prefers-color-scheme: dark)')
         .matches;
     isSystemMode: boolean = false;
@@ -28,23 +28,16 @@ export class DarkModeService {
             this.DARK_MODE_SYSTEM_KEY
         );
 
-        //if store system is set to true
         if (storedSystemMode === 'true') {
-            //set issytem mode to true
             this.isSystemMode = true;
-            //we set darkmode based on system theme mode
             this.isDarkMode = window.matchMedia(
                 '(prefers-color-scheme: dark)'
             ).matches;
         } else if (storedDarkMode) {
-            // nqs dark storedark mode ne localstorage is true vendosim system mode to false
-            //also we set darkmode based on value of storedarkmode this one is darkmode
             this.isDarkMode = storedDarkMode === 'true';
             this.isSystemMode = false;
         } else {
-            //here we set system mode to true as default
             this.isSystemMode = true;
-            //and save the dark mode system key to local storage
             localStorage.setItem(this.DARK_MODE_SYSTEM_KEY, 'true');
         }
 
@@ -62,10 +55,9 @@ export class DarkModeService {
         localStorage.setItem(this.DARK_MODE_SYSTEM_KEY, 'true');
         localStorage.removeItem(this.DARK_MODE_KEY);
 
-        const prefersDark = window.matchMedia(
+        this.isDarkMode = window.matchMedia(
             '(prefers-color-scheme: dark)'
         ).matches;
-        this.isDarkMode = prefersDark;
 
         this.onDarkModeChange();
     }
@@ -76,21 +68,20 @@ export class DarkModeService {
         this.saveThemePreference();
     }
 
-    saveThemePreference(): void {
+    private saveThemePreference(): void {
         localStorage.setItem(this.DARK_MODE_KEY, String(this.isDarkMode));
         localStorage.setItem(this.DARK_MODE_SYSTEM_KEY, 'false');
         this.onDarkModeChange();
     }
 
-    addTransitionClass(): void {
+    private addTransitionClass(): void {
         document
             .querySelectorAll('.dark-mode-slider')
             .forEach((slider) => slider.classList.add('transition-enabled'));
     }
 
-    onDarkModeChange(): void {
+    private onDarkModeChange(): void {
         this.addTransitionClass();
-
         localStorage.setItem(this.DARK_MODE_KEY, String(this.isDarkMode));
     }
 }
