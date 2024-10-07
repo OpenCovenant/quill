f482b02893d804d57a62558add61ed15334c65d5
Anderson Ceci <andersonceci31@gmail.com>
2024-10-07 14:59:43 +0200

    made hover on dark mode buttons also added seperate code for theme buttons

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -6,13 +6,14 @@ import { DarkModeService } from '../services/dark-mode.service';
 import { Router, RouterModule } from '@angular/router';
 import { AuthenticationService } from '../services/authentication.service';
 import { CommonModule } from '@angular/common';
+import {DarkModeButtonsComponent} from "../settings/dark-mode-buttons/dark-mode-buttons.component";
 
 @Component({
     standalone: true,
     selector: 'app-header',
     templateUrl: './header.component.html',
     styleUrls: ['./header.component.css'],
-    imports: [CommonModule, RouterModule]
+    imports: [CommonModule, RouterModule, DarkModeButtonsComponent]
 })
 export class HeaderComponent {
     baseURL!: string;
